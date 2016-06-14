96b492313e3aa1a54e40b010b3cd1972391d1026
David Moulton <davidcmoulton@users.noreply.github.com>
2016-06-14 15:59:14 +0100

    Feature/viewmodel/teaser non article content (#1)
    * Add beginnings of view model for teaser non-article content
    
    Todo: tests for date; add more tests for pasring of json schema for required fields; add more tests for input/output of required fields; add optional fields; add tests for optional fields;
    
    * headerText must be non-empty string (test & implementation)
    
    * link must be non-empty string (test & implementation)
    
    * content must be non-empty string (test & implementation)
    
    * Refactor exception tests to use method instead of decorator
    
    * Add optional subheader, must be non-empty string if supplied
    
    * Add optional footer text, must be non-empty string if supplied
    
    * Move this view model's self-validation to dedicated method
    
    * Add optional download source, must be non-empty string if supplied
    
    * Tidy up code
    
    * Address comments incl. refector testing apprach
    
    * Fix coding standards bugs

---
+++ src/ViewModel/Date.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use DateTimeImmutable;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\CastsToArray;
+use eLife\Patterns\ReadOnlyArrayAccess;
+
+final class Date implements CastsToArray
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+
+    private $forHuman;
+    private $forMachine;
+
+    public function __construct(DateTimeImmutable $date)
+    {
+        $this->forHuman = $date->format('M j, Y');
+        $this->forMachine = $date->format('Y-m-d');
+    }
+}
---
+++ src/ViewModel/TeaserNonArticleContent.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class TeaserNonArticleContent implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    private $content;
+    private $date;
+    private $headerText;
+    private $link;
+
+    public function __construct(string $content, Date $date, string $headerText, string $link, string $subHeader = null,
+                                string $footerText = null, string $downloadSrc = null)
+    {
+        Assertion::notBlank($content);
+        Assertion::notBlank($headerText);
+        Assertion::notBlank($link);
+
+        Assertion::nullOrNotBlank($subHeader);
+        Assertion::nullOrNotBlank($footerText);
+        Assertion::nullOrNotBlank($downloadSrc);
+
+        $this->content = $content;
+        $this->date = $date;
+        $this->headerText = $headerText;
+        $this->link = $link;
+        $this->subHeader = $subHeader;
+        $this->footerText = $footerText;
+        $this->downloadSrc = $downloadSrc;
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield '/elife/patterns/assets/css/teaser--non-article-content.css';
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/teaser--non-article-content.mustache';
+    }
+}
