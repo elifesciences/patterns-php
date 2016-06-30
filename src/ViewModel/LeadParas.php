3060995337de6786c565c2c4037a8210cc19fd48
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-06-30 16:47:24 +0100

    ELPP-1115 Added lead para view model and test

---
+++ src/ViewModel/LeadParas.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+class LeadParas implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    protected $paras;
+
+    public function __construct($leadParas)
+    {
+        Assertion::notEmpty($leadParas);
+        Assertion::greaterThan(count($leadParas), 1);
+        Assertion::allIsInstanceOf($leadParas, LeadPara::class);
+
+        $this->paras = $leadParas;
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield '/elife/patterns/assets/css/lead-paras.css';
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/lead-paras.mustache';
+    }
+}
