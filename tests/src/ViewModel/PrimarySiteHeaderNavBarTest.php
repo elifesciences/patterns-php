c1179a9ae780c41d344a5679962a25316d773e19
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-07-01 14:45:26 +0100

    Refactor class creation for the site header

--- tests/src/ViewModel/PrimarySiteHeaderNavBarTest.php
+++ tests/src/ViewModel/PrimarySiteHeaderNavBarTest.php
@@ -3,6 +3,7 @@
 namespace tests\eLife\Patterns\ViewModel;
 
 use eLife\Patterns\ViewModel\Image;
+use eLife\Patterns\ViewModel\Link;
 use eLife\Patterns\ViewModel\NavLinkedItem;
 use eLife\Patterns\ViewModel\PictureSvgWithFallback;
 use eLife\Patterns\ViewModel\SiteHeaderNavBar;
@@ -25,9 +26,9 @@ final class PrimarySiteHeaderNavBarTest extends ViewModelTest
           ],
           new Image('/path/to/fallback/', [500 => '/path/in/srcset'], 'alt text', [])
         );
-        $this->linkItem1 = NavLinkedItem::asLink('item 1', '/item-1/', ['class-1'], ['text-class-1'], false, $this->img);
-        $this->linkItem2 = NavLinkedItem::asLink('item 2', '/item-2/', ['class-2'], ['text-class-2'], true);
-        $this->linkItem3 = NavLinkedItem::asLink('item 2', '/item-2/', ['class-2'], ['text-class-2'], false);
+        $this->linkItem1 = NavLinkedItem::asIcon(new Link('item 1', '/item-1/'), $this->img, false);
+        $this->linkItem2 = NavLinkedItem::asLink(new Link('item 2', '/item-2/'), true);
+        $this->linkItem3 = NavLinkedItem::asLink(new Link('item 2', '/item-2/'), false);
         $this->linkItems = [$this->linkItem1, $this->linkItem2, $this->linkItem3];
         $this->siteHeaderNavBar = SiteHeaderNavBar::primary($this->linkItems);
     }
@@ -41,19 +42,19 @@ final class PrimarySiteHeaderNavBarTest extends ViewModelTest
 
         $this->assertSame($this->linkItem1['text'], $siteHeaderNavItems[0]['text']);
         $this->assertSame($this->linkItem1['path'], $siteHeaderNavItems[0]['path']);
-        $this->assertSame($this->linkItem1['classes'], $siteHeaderNavItems[0]['classes']);
+        $this->assertSame('nav-primary__item nav-primary__item--first', $siteHeaderNavItems[0]['classes']);
         $this->assertSame($this->linkItem1['rel'], $siteHeaderNavItems[0]['rel']);
-        $this->assertSame($this->linkItem1['img'], $siteHeaderNavItems[0]['img']);
+        $this->assertSame($this->linkItem1['img']->toArray(), $siteHeaderNavItems[0]['img']);
 
         $this->assertSame($this->linkItem2['text'], $siteHeaderNavItems[1]['text']);
         $this->assertSame($this->linkItem2['path'], $siteHeaderNavItems[1]['path']);
-        $this->assertSame($this->linkItem2['classes'], $siteHeaderNavItems[1]['classes']);
+        $this->assertSame('nav-primary__item', $siteHeaderNavItems[1]['classes']);
         $this->assertSame($this->linkItem2['rel'], $siteHeaderNavItems[1]['rel']);
         $this->assertSame($this->linkItem2['img'], $siteHeaderNavItems[1]['img']);
 
         $this->assertSame($this->linkItem3['text'], $siteHeaderNavItems[2]['text']);
         $this->assertSame($this->linkItem3['path'], $siteHeaderNavItems[2]['path']);
-        $this->assertSame($this->linkItem3['classes'], $siteHeaderNavItems[2]['classes']);
+        $this->assertSame('nav-primary__item nav-primary__item--last', $siteHeaderNavItems[2]['classes']);
         $this->assertSame($this->linkItem3['rel'], $siteHeaderNavItems[2]['rel']);
         $this->assertSame($this->linkItem3['img'], $siteHeaderNavItems[2]['img']);
     }
@@ -84,8 +85,8 @@ final class PrimarySiteHeaderNavBarTest extends ViewModelTest
         );
 
         $navLinkItems = [
-          NavLinkedItem::asLink('item 1', '/item-1/', ['class-1'], ['text-class-1'], false, $img),
-          NavLinkedItem::asLink('item 2', '/item-2/', ['class-2'], ['text-class-2'], false),
+          NavLinkedItem::asIcon(new Link('item 1', '/item-1/'), $img, false),
+          NavLinkedItem::asLink(new Link('item 2', '/item-2/'), false),
         ];
 
         return [
