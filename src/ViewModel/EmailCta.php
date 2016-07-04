90a8db52220775bf80470def64f359bb29e764f2
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-07-04 14:39:38 +0100

    ELPP-1139 Added EmailCta view model and tests

---
+++ src/ViewModel/EmailCta.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+class EmailCta implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    protected $headerText;
+    protected $subHeader;
+    protected $compactForm;
+
+    public function __construct(
+        string $headerText,
+        string $subHeader,
+        CompactForm $compactForm
+    ) {
+        Assertion::notBlank($headerText);
+        Assertion::notBlank($subHeader);
+
+        $this->headerText = $headerText;
+        $this->subHeader = $subHeader;
+        $this->compactForm = $compactForm;
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield '/elife/patterns/assets/css/lead-para.css';
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/email-cta.mustache';
+    }
+}
