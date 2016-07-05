46cc7f283ba9655fe39958dd9f70bc2b062e3143
Stephen Fraser <stephenwf@users.noreply.github.com>
2016-07-05 17:01:13 +0200

    ELPP-1093 Captioned image view model (#34)
    * ELPP-1093 Added captioned image view mode and tests
    
    * ELPP-1093 Added captioned image view model and tests
    
    * ELPP-1093 Added captioned image yaml and template
    
    * ELPP-1093 Fixed YAML on captioned image
    
    * ELPP-1093 Captioned Image protected to private change

---
+++ src/ViewModel/CaptionedImage.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class CaptionedImage implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    private $heading;
+    private $captions;
+    private $picture;
+    private $customContent;
+
+    private function __construct(Picture $picture, string $heading = null, array $captions = null, string $customContent = null)
+    {
+        $this->heading = $heading;
+        $this->captions = $captions;
+        $this->picture = $picture;
+        $this->customContent = $customContent;
+    }
+
+    public static function withParagraph(Picture $picture, string $heading, string $caption) : CaptionedImage
+    {
+        Assertion::notBlank($heading);
+        Assertion::notBlank($caption);
+
+        return new static($picture, $heading, [['caption' => $caption]]);
+    }
+
+    public static function withParagraphs(Picture $picture, string $heading, array $captions) : CaptionedImage
+    {
+        Assertion::notBlank($heading);
+        Assertion::notBlank($captions);
+        Assertion::allString($captions);
+
+        $captions = array_map(function ($caption) {
+            return ['caption' => $caption];
+        }, $captions);
+
+        return new static($picture, $heading, $captions);
+    }
+
+    public static function withCustomContent(Picture $picture, string $content) : CaptionedImage
+    {
+        Assertion::notBlank($content);
+
+        return new static($picture, null, null, $content);
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield '/elife/patterns/assets/css/captioned-image.css';
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/captioned-image.mustache';
+    }
+}
