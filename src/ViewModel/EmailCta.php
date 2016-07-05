101e6163fa9325e441c6dfff224513bbe748b442
Stephen Fraser <stephenwf@users.noreply.github.com>
2016-07-05 17:19:33 +0200

    ELPP-1139 Email CTA View models (#32)
    * ELPP-1139 Added EmailCta view model and tests
    
    * ELPP-1139 Added EmailCta view model and tests [fix tests]
    
    * ELPP-1139 Fixed code style
    
    * ELPP-1139 Added EmailCta view model and tests [fix tests 2]
    
    * ELPP-1139 Added final to classes
    
    * ELPP-1139 Removed assertion
    
    * ELPP-1139 Revert wip change
    
    * ELPP-1139 Email CTA change from protected -> private
    
    * ELPP-1139 Added subHeader as required field into Email CTA
    
    * ELPP-1139 Added subHeader as required field into Email CTA [rev]

---
+++ src/ViewModel/EmailCta.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class EmailCta implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    private $headerText;
+    private $subHeader;
+    private $compactForm;
+
+    public function __construct(
+        string $headerText,
+        string $subHeader,
+        CompactForm $compactForm
+    ) {
+        Assertion::notBlank($headerText);
+        Assertion::notBlank($subHeader);
+
+        $this->headerText = $headerText;
+        $this->subHeader = $subHeader;
+        $this->compactForm = $compactForm;
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield $this->compactForm->getStyleSheets();
+        yield '/elife/patterns/assets/css/email-cta.css';
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/email-cta.mustache';
+    }
+}
