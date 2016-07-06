e3fae2d0459412894d520b3e52b48bfb1baf0d38
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-07-06 14:02:34 +0100

    ELPP-1119 Added MiniSection ViewModel and tests

---
+++ src/ViewModel/MiniSection.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class MiniSection implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    private $body;
+    private $listHeading;
+
+    public function __construct(string $body, ListHeading $listHeading = null)
+    {
+        Assertion::notBlank($body);
+
+        $this->body = $body;
+        $this->listHeading = $listHeading;
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/mini-section.mustache';
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield '/elife/patterns/assets/css/mini-section.css';
+        if ($this->listHeading) {
+            yield $this->listHeading->getStyleSheets();
+        }
+    }
+}
