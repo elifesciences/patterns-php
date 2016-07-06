2a0711e0e43e230e776185dfca46fc1bab9d5aee
Stephen Fraser <stephenwf@users.noreply.github.com>
2016-07-06 13:10:03 +0200

    ELPP-1128 Added ViewerInline ViewModel and Tests (#37)
    * ELPP-1128 Added ViewerInline ViewModel and Tests
    
    * ELPP-1128 Added Viewer inline YAML and Template
    
    * ELPP-1128 Fixed ID on ViewerInline and tests

---
+++ src/ViewModel/ViewerInline.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class ViewerInline implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    private $id;
+    private $prominentText;
+    private $normalText;
+    private $seeAllLink;
+    private $downloadLink;
+    private $newWindowLink;
+    private $captionedImage;
+
+    public function __construct(
+        string $prominentText,
+        string $normalText,
+        string $seeAllLink,
+        string $downloadLink,
+        string $newWindowLink,
+        CaptionedImage $captionedImage
+    )
+    {
+        Assertion::notBlank($prominentText);
+        Assertion::notBlank($normalText);
+        Assertion::notBlank($seeAllLink);
+        Assertion::notBlank($downloadLink);
+        Assertion::notBlank($newWindowLink);
+
+        $this->id = 'viewerInline' . hash('crc32', $prominentText . $normalText . $downloadLink);
+        $this->prominentText = $prominentText;
+        $this->normalText = $normalText;
+        $this->seeAllLink = $seeAllLink;
+        $this->downloadLink = $downloadLink;
+        $this->newWindowLink = $newWindowLink;
+        $this->captionedImage = $captionedImage;
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield '/elife/patterns/assets/css/viewer-inline.css';
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/viewer-inline.mustache';
+    }
+}
