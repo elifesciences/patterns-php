a1ab08333ac8a525c6899cb59e970394865e1a99
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-07-06 16:57:30 +0100

    ELPP-1107 Fixed typo

--- tests/src/ViewModel/ArticleDownloadLinksListTest.php
+++ tests/src/ViewModel/ArticleDownloadLinksListTest.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace tests\eLife\Patterns\ViewModel;
-
-use eLife\Patterns\ViewModel\ArticleDownloadsLinksList;
+use eLife\Patterns\ViewModel\ArticleDownloadLinksList;
 
 class ArticleDownloadLinksListTest extends ViewModelTest
 {
@@ -28,7 +27,7 @@ class ArticleDownloadLinksListTest extends ViewModelTest
             'linkCitsCiteULike' => '#12',
         ];
 
-        $downloadList = new ArticleDownloadsLinksList(
+        $downloadList = new ArticleDownloadLinksList(
             $data['dlLinkArticle'],
             $data['dlLinkFigure'],
             $data['dlLinkCitsBibtex'],
@@ -61,7 +60,7 @@ class ArticleDownloadLinksListTest extends ViewModelTest
     public function viewModelProvider() : array
     {
         return [
-            [new ArticleDownloadsLinksList('#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8', '#9', '#10', '#11', '#12')]
+            [new ArticleDownloadLinksList('#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8', '#9', '#10', '#11', '#12')]
         ];
     }
 
