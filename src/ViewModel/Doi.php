a9bd72b50f7a08da27be1f7046f49e93bef21558
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-07-06 15:40:08 +0100

    ELPP-1094 Added Doi ViewModel and tests

---
+++ src/ViewModel/Doi.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class Doi implements ViewModel
+{
+
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    private $uri;
+    private $classNames;
+
+    public function __construct(string $uri, array $classNames = [])
+    {
+        Assertion::notBlank($uri);
+
+        $this->uri = $uri;
+        $this->classNames = implode(' ', $classNames);
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/doi.mustache';
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield '/elife/patterns/assets/css/doi.css';
+    }
+
+}
