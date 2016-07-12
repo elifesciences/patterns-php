390e9d183eed9cf707e72ac0e4a13c374840614f
Stephen Fraser <stephenwf@users.noreply.github.com>
2016-07-12 16:52:39 +0100

    ELPP-1121 Added PullQuote ViewModel and tests (#42)
    * ELPP-1121 Added PullQuote ViewModel and tests
    
    * ELPP-1121 Added PullQuote yaml and template
    
    * ELPP-1121 Added pull quote json
    
    * ELPP-1121 Fixed code style

---
+++ tests/src/ViewModel/PullQuoteTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace tests\eLife\Patterns\ViewModel;
+
+use eLife\Patterns\ViewModel\PullQuote;
+
+final class PullQuoteTest extends ViewModelTest
+{
+    /**
+     * @test
+     */
+    public function it_has_data()
+    {
+        $data = [
+            'quote' => 'quote',
+            'cite' => 'cite <a href="#">with link</a>',
+            'asPara' => true,
+        ];
+        $pullQuote = new PullQuote('quote', 'cite <a href="#">with link</a>');
+
+        $this->assertSame($data['quote'], $pullQuote['quote']);
+        $this->assertSame($data['cite'], $pullQuote['cite']);
+        $this->assertSame($data, $pullQuote->toArray());
+    }
+
+    public function viewModelProvider() : array
+    {
+        return [
+            'with quote and cite' => [new PullQuote('quote', 'cite')],
+            'with links in cite' => [new PullQuote('quote', 'cite <a href="#">with link</a>')],
+            'with links in cite not paragraph' => [new PullQuote('quote', 'cite <a href="#">with link</a>', false)],
+        ];
+    }
+
+    protected function expectedTemplate() : string
+    {
+        return '/elife/patterns/templates/pull-quote.mustache';
+    }
+}
