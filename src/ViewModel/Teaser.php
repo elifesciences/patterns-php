04596b2c2b6ea43c4c082d8394286810a5b13cfe
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-07-26 17:54:36 +0100

    ELPP-1133 Added first pass of teasers view model

---
+++ src/ViewModel/Teaser.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class Teaser implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    private $title;
+    private $rootClasses;
+    private $url;
+    private $content;
+    private $secondaryInfo;
+    private $contextLabel;
+    private $eventDate;
+    private $category;
+    private $image;
+    private $footer;
+
+    protected function __construct(
+        string $title,
+        string $rootClasses = null,
+        string $url = null,
+        string $content = null,
+        string $secondaryInfo = null,
+        SiteLinks $contextLabel = null,
+        Date $eventDate = null,
+        Link $category = null,
+        TeaserImage $image = null,
+        /* TeaserFooter */ $footer = null
+    ) {
+        Assertion::notBlank($title);
+
+        $this->title = $title;
+        $this->rootClasses = $rootClasses;
+        $this->url = $url;
+        $this->content = $content;
+        $this->secondaryInfo = $secondaryInfo;
+        $this->contextLabel = $contextLabel;
+        $this->eventDate = $eventDate;
+        $this->category = $category;
+        $this->image = $image;
+        $this->footer = $footer;
+    }
+
+    public static function basic($title)
+    {
+        return new static($title);
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/teaser.mustache';
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield '/elife/patterns/assets/css/teaser.css';
+    }
+}
---
+++ src/ViewModel/TeaserImage.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\CastsToArray;
+use eLife\Patterns\ReadOnlyArrayAccess;
+
+final class TeaserImage implements CastsToArray
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+
+    private $defaultPath;
+    private $altText;
+    private $url;
+    private $srcset;
+    private $classes;
+
+    const STYLE_PROMINENT = 'teaser__img--prominent';
+    const STYLE_BIG = 'teaser__img--big';
+    const STYLE_SMALL = 'teaser__img--small';
+
+    public function __construct(
+        string $defaultPath,
+        string $altText,
+        string $url = null,
+        array $srcset = null,
+        array $classes = null
+    ) {
+        Assertion::notBlank($defaultPath);
+        Assertion::notBlank($altText);
+
+        $this->defaultPath = $defaultPath;
+        $this->altText = $altText;
+        $this->url = $url;
+        $this->srcset = $this->srcsetFromArray($srcset);
+        if ($classes) {
+            Assertion::allInArray($classes, [self::STYLE_PROMINENT, self::STYLE_BIG, self::STYLE_SMALL]);
+            $this->classes = implode(' ', $classes);
+        }
+    }
+
+    public static function prominent(
+        string $defaultPath,
+        string $altText,
+        string $url = null,
+        array $srcset = null
+    ) {
+        return new static (
+            $defaultPath,
+            $altText,
+            $url,
+            $srcset,
+            [self::STYLE_PROMINENT]
+        );
+    }
+
+    public static function big(
+        string $defaultPath,
+        string $altText,
+        string $url = null,
+        array $srcset = null
+    ) {
+        return new static (
+            $defaultPath,
+            $altText,
+            $url,
+            $srcset,
+            [self::STYLE_BIG]
+        );
+    }
+
+    public static function small(
+        string $defaultPath,
+        string $altText,
+        string $url = null,
+        array $srcset = null
+    ) {
+        return new static (
+            $defaultPath,
+            $altText,
+            $url,
+            $srcset,
+            [self::STYLE_SMALL]
+        );
+    }
+
+    private function srcsetFromArray(array $srcset = null)
+    {
+        return $srcset === null ?
+            null :
+            implode(', ', array_map(function ($src, $width) {
+                return $src.' '.$width.'w';
+            }, $srcset));
+    }
+}
