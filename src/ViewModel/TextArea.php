109c42b35e4c766471db30460ed5581e800155be
Stephen Fraser <stephenwf@users.noreply.github.com>
2016-08-03 14:49:49 +0100

    ELPP-1289 Added text area ViewModel and tests (#88)
    * ELPP-1289 Added text area and tests
    
    * ELPP-1289 Fixed tests with new properties
    
    * ELPP-1289 Added text area assets
    
    * ELPP-1289 Fixed CS :ghost:

---
+++ src/ViewModel/TextArea.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class TextArea implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    private $label;
+    private $name;
+    private $id;
+    private $value;
+
+    public function __construct(
+        FormLabel $label,
+        string $id,
+        string $name,
+        string $value = null,
+        string $placeholder = null,
+        bool $required = null,
+        bool $disabled = null,
+        bool $autofocus = null,
+        int $cols = null,
+        int $rows = null,
+        string $form = null
+    ) {
+        Assertion::same($id, $label['for']);
+
+        $this->label = $label;
+        $this->name = $name;
+        $this->id = $id;
+        $this->value = $value;
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/text-area.mustache';
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield '/elife/patterns/assets/css/text-fields.css';
+    }
+}
