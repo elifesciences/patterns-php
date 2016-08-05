1dbc8e11c73e16f4d5bce62ccd52dffbfd27b504
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-08-05 11:42:44 +0100

    Shame shame shame :bell:

--- src/ViewModel/Listing.php
+++ src/ViewModel/Listing.php
@@ -8,6 +8,7 @@ use eLife\Patterns\ReadOnlyArrayAccess;
 use eLife\Patterns\SimplifyAssets;
 use eLife\Patterns\ViewModel;
 use Traversable;
+use ReflectionClass;
 
 final class Listing implements ViewModel
 {
@@ -15,24 +16,20 @@ final class Listing implements ViewModel
     use ReadOnlyArrayAccess;
     use SimplifyAssets;
 
+    const PROFILE_SNIPPETS = 'isProfileSnippetType';
+    const TEASERS = 'isTeaserType';
+
     private $seeMoreLink;
     private $loadMore;
-    /**
-     * @var string
-     */
     private $heading;
-    /**
-     * @var array
-     */
     private $items;
-    /**
-     * @var ListingItem
-     */
-    private $listingItem;
 
-    private function __construct(string $heading, array $items, ListingItem $listingItem)
+    private function __construct(string $heading, array $items, ListingItem $listingItem, string $typeProperty)
     {
         Assertion::notEmpty($items);
+        Assertion::inArray($typeProperty, [self::PROFILE_SNIPPETS, self::TEASERS]);
+
+        $this->{$typeProperty} = true;
         $this->{$listingItem->getProperty()} = $listingItem->getItem();
         $this->heading = $heading;
         $this->items = $items;
@@ -40,12 +37,12 @@ final class Listing implements ViewModel
 
     public static function withProfileSnippets($heading, ListingItem $listingItem, ProfileSnippet ...$items)
     {
-        return new static($heading, $items, $listingItem);
+        return new static($heading, $items, $listingItem, self::PROFILE_SNIPPETS);
     }
 
     public static function withTeasers($heading, ListingItem $listingItem, Teaser ...$items)
     {
-        return new static($heading, $items, $listingItem);
+        return new static($heading, $items, $listingItem, self::TEASERS);
     }
 
     public function getTemplateName() : string
@@ -53,8 +50,22 @@ final class Listing implements ViewModel
         return '/elife/patterns/templates/listing.mustache';
     }
 
+    /**
+     * @todo Slight technical debt here. I would maybe suggest making the get* methods static.
+     *
+     * @return Traversable
+     */
     public function getStyleSheets() : Traversable
     {
         yield '/elife/patterns/assets/css/listing.css';
+        yield from ((new ReflectionClass(ProfileSnippet::class))->newInstanceWithoutConstructor()->getStyleSheets());
+        yield from ((new ReflectionClass(SeeMoreLink::class))->newInstanceWithoutConstructor()->getStyleSheets());
+        yield from ((new ReflectionClass(Teaser::class))->newInstanceWithoutConstructor()->getStyleSheets());
+        $ref = (new ReflectionClass(Button::class));
+        $button = $ref->newInstanceWithoutConstructor();
+        $prop = $ref->getProperty('templateName');
+        $prop->setAccessible(true);
+        $prop->setValue($button, 'load-more');
+        yield from ($button->getStyleSheets());
     }
 }
