4a5ef50990a96ce8a64237fd136934733c14fb02
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-08-09 15:22:26 +0100

    Fixed button

---
+++ src/ViewModel/LoadMoreButton.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class LoadMoreButton implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    private $classes;
+    private $path;
+    private $text;
+    private $type;
+
+    public function __construct(
+        string $text,
+        string $path,
+        string $size = Button::SIZE_MEDIUM,
+        string $style = Button::STYLE_DEFAULT,
+        bool $isActive = true,
+        bool $isFullWidth = false
+    ) {
+        $button = Button::link($text, $path, $size, $style, $isActive, $isFullWidth);
+        $this->classes = $button['classes'];
+        $this->path = $button['path'];
+        $this->text = $button['text'];
+        $this->type = $button['type'];
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/load-more.mustache';
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield '/elife/patterns/assets/css/buttons.css';
+        yield '/elife/patterns/assets/css/load-more.css';
+    }
+}
