c796e8b995faa84565e9e4d7859fed458e50c133
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-09-21 16:05:49 +0100

    Add block link (#119)

---
+++ src/ViewModel/BlockLink.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class BlockLink implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    private $text;
+    private $url;
+    private $behaviour;
+    private $backgroundImage = false;
+    private $lowResImageSource;
+    private $highResImageSource;
+
+    public function __construct(Link $link, BackgroundImage $backgroundImage = null)
+    {
+        $this->text = $link['name'];
+        $this->url = $link['url'];
+        if ($backgroundImage) {
+            $this->behaviour = 'BackgroundImage';
+            $this->backgroundImage = true;
+            $this->lowResImageSource = $backgroundImage['lowResImageSource'];
+            $this->highResImageSource = $backgroundImage['highResImageSource'];
+        }
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/block-link.mustache';
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield '/elife/patterns/assets/css/block-link.css';
+    }
+}
