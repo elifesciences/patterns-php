01153fa9638615c2ae2589301f88652d91ec5936
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-09-22 14:59:14 +0100

    Match menu changes (#121)

--- tests/src/ViewModel/FooterTest.php
+++ tests/src/ViewModel/FooterTest.php
@@ -5,7 +5,6 @@ namespace tests\eLife\Patterns\ViewModel;
 use eLife\Patterns\ViewModel\Footer;
 use eLife\Patterns\ViewModel\Link;
 use eLife\Patterns\ViewModel\MainMenu;
-use eLife\Patterns\ViewModel\MainMenuLink;
 
 final class FooterTest extends ViewModelTest
 {
@@ -16,29 +15,27 @@ final class FooterTest extends ViewModelTest
     {
         $data = [
             'year' => (int) date('Y'),
-            'mainMenuLinks' => [
-                [
-                    'title' => 'title',
-                    'titleId' => 'mainMenu'.hash('crc32', 'title'),
-                    'items' => [['name' => 'name1', 'url' => 'url1']],
-                ],
+            'mainMenu' => true,
+            'links' => ['items' => [['name' => 'name1', 'url' => 'url1']]],
+            'button' => [
+                'classes' => 'button--small button--default button--full main_menu__quit',
+                'path' => '#siteHeader',
+                'text' => 'Back to top',
             ],
             'footerMenuLinks' => [['name' => 'name2', 'url' => 'url2']],
         ];
 
         $footer = new Footer(
-            new MainMenu($mainMenuLinks = [
-                new MainMenuLink($data['mainMenuLinks'][0]['title'],
-                    $links = [
-                        new Link($data['mainMenuLinks'][0]['items'][0]['name'],
-                            $data['mainMenuLinks'][0]['items'][0]['url']),
-                    ]),
+            new MainMenu($links = [
+                new Link($data['links']['items'][0]['name'], $data['links']['items'][0]['url']),
             ]),
             $footerMenuLinks = [new Link($data['footerMenuLinks'][0]['name'], $data['footerMenuLinks'][0]['url'])]
         );
 
         $this->assertSame($data['year'], $footer['year']);
-        $this->assertEquals($mainMenuLinks, $footer['mainMenuLinks']);
+        $this->assertSame($data['mainMenu'], $footer['mainMenu']);
+        $this->assertEquals($links, $footer['links']['items']);
+        $this->assertSame($data['button'], $footer['button']->toArray());
         $this->assertEquals($footerMenuLinks, $footer['footerMenuLinks']);
         $this->assertSame($data, $footer->toArray());
     }
@@ -47,7 +44,7 @@ final class FooterTest extends ViewModelTest
     {
         return [
             [
-                new Footer(new MainMenu([new MainMenuLink('title', [new Link('name1', 'url1')])]),
+                new Footer(new MainMenu([new Link('name1', 'url1')]),
                     [new Link('name2', 'url2')], [new Link('name3', 'url3')]),
             ],
         ];
