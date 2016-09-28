f64ae27a31b9a16116800e77977a85a7cd49ff97
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-09-28 13:06:52 +0100

    Match DOI updates

--- src/ViewModel/ArticleSection.php
+++ src/ViewModel/ArticleSection.php
@@ -4,18 +4,20 @@ namespace eLife\Patterns\ViewModel;
 
 use Assert\Assertion;
 use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ComposedAssets;
 use eLife\Patterns\ReadOnlyArrayAccess;
-use eLife\Patterns\SimplifyAssets;
 use eLife\Patterns\ViewModel;
+use InvalidArgumentException;
 use Traversable;
 
 final class ArticleSection implements ViewModel
 {
     use ArrayFromProperties;
+    use ComposedAssets;
     use ReadOnlyArrayAccess;
-    use SimplifyAssets;
 
     private $id;
+    private $doi;
     private $title;
     private $headingLevel;
     private $hasBehaviour;
@@ -25,6 +27,7 @@ final class ArticleSection implements ViewModel
 
     private function __construct(
         string $id = null,
+        Doi $doi = null,
         string $title,
         int $headingLevel,
         string $body,
@@ -37,7 +40,16 @@ final class ArticleSection implements ViewModel
         Assertion::max($headingLevel, 6);
         Assertion::notBlank($body);
 
+        if (null === $id && $doi) {
+            throw new InvalidArgumentException('DOI requires an ID');
+        }
+
+        if ($doi) {
+            $doi = FlexibleViewModel::fromViewModel($doi)->withProperty('variant', 'article-section');
+        }
+
         $this->id = $id;
+        $this->doi = $doi;
         $this->title = $title;
         $this->headingLevel = $headingLevel;
         $this->hasBehaviour = $hasBehaviour;
@@ -51,9 +63,10 @@ final class ArticleSection implements ViewModel
         int $headingLevel,
         string $body,
         $id = null,
+        Doi $doi = null,
         bool $isFirst = false
     ) : ArticleSection {
-        return new self($id, $title, $headingLevel, $body, $isFirst);
+        return new self($id, $doi, $title, $headingLevel, $body, $isFirst);
     }
 
     public static function collapsible(
@@ -62,9 +75,10 @@ final class ArticleSection implements ViewModel
         int $headingLevel,
         string $body,
         bool $isInitiallyClosed = false,
-        bool $isFirst = false
+        bool $isFirst = false,
+        Doi $doi = null
     ) : ArticleSection {
-        return new self($id, $title, $headingLevel, $body, $isFirst, true, $isInitiallyClosed);
+        return new self($id, $doi, $title, $headingLevel, $body, $isFirst, true, $isInitiallyClosed);
     }
 
     public function getTemplateName() : string
@@ -72,8 +86,13 @@ final class ArticleSection implements ViewModel
         return '/elife/patterns/templates/article-section.mustache';
     }
 
-    public function getStyleSheets() : Traversable
+    protected function getLocalStyleSheets() : Traversable
     {
         yield '/elife/patterns/assets/css/article-section.css';
+    }
+
+    protected function getComposedViewModels() : Traversable
+    {
+        yield $this->doi;
     }
 }
--- tests/src/ViewModel/DoiTest.php
+++ tests/src/ViewModel/DoiTest.php
@@ -3,6 +3,7 @@
 namespace tests\eLife\Patterns\ViewModel;
 
 use eLife\Patterns\ViewModel\Doi;
+use InvalidArgumentException;
 
 final class DoiTest extends ViewModelTest
 {
@@ -12,21 +13,25 @@ final class DoiTest extends ViewModelTest
     public function it_has_data()
     {
         $data = [
-            'uri' => 'http://dx.doi.org/10.7554/eLife.10181.001',
-            'classNames' => 'class1 class2',
+            'doi' => '10.7554/eLife.10181.001',
         ];
-        $doi = new Doi($data['uri'], explode(' ', $data['classNames']));
+        $doi = new Doi($data['doi']);
 
-        $this->assertSame($data['uri'], $doi['uri']);
-        $this->assertSame($data['classNames'], $doi['classNames']);
+        $this->assertSame($data['doi'], $doi['doi']);
         $this->assertSame($data, $doi->toArray());
+    }
+
+    public function it_must_be_a_doi()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new Doi('foo');
     }
 
     public function viewModelProvider() : array
     {
         return [
-            'with class names' => [new Doi('http://dx.doi.org/10.7554/eLife.10181.001', ['class1', 'class2'])],
-            'without class names' => [new Doi('http://dx.doi.org/10.7554/eLife.10181.001')],
+            [new Doi('10.7554/eLife.10181.001')],
         ];
     }
 
