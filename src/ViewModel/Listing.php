b521ee20483b98e2d28bf91934d285a2e49f82cd
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-10-27 14:47:47 +0100

    Add listing (#143)

---
+++ src/ViewModel/Listing.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class Listing implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    private $isOrdered;
+    private $prefix;
+    private $items;
+
+    private function __construct(bool $isOrdered, string $prefix = null, array $items)
+    {
+        Assertion::nullOrChoice($prefix,
+            ['alpha-lower', 'alpha-upper', 'bullet', 'number', 'roman-lower', 'roman-upper']);
+        Assertion::notEmpty($items);
+        Assertion::allString($items);
+
+        $this->isOrdered = $isOrdered;
+        $this->prefix = $prefix;
+        $this->items = $items;
+    }
+
+    public static function ordered(array $items, string $prefix = null) : Listing
+    {
+        return new self(true, $prefix, $items);
+    }
+
+    public static function unordered(array $items, string $prefix = null) : Listing
+    {
+        return new self(false, $prefix, $items);
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield '/elife/patterns/assets/css/list.css';
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/patterns/templates/list.mustache';
+    }
+}
---
+++ tests/src/ViewModel/ListingTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace tests\eLife\Patterns\ViewModel;
+
+use eLife\Patterns\ViewModel\Listing;
+use InvalidArgumentException;
+
+final class ListingTest extends ViewModelTest
+{
+    /**
+     * @test
+     */
+    public function it_has_data()
+    {
+        $data = [
+            'isOrdered' => true,
+            'prefix' => 'roman-upper',
+            'items' => ['foo', 'bar'],
+        ];
+
+        $listing = Listing::ordered(['foo', 'bar'], 'roman-upper');
+
+        $this->assertSame($data['isOrdered'], $listing['isOrdered']);
+        $this->assertSame($data['prefix'], $listing['prefix']);
+        $this->assertSame($data['items'], $listing['items']);
+        $this->assertSame($data, $listing->toArray());
+
+        $data = [
+            'isOrdered' => false,
+            'items' => ['foo', 'bar'],
+        ];
+
+        $listing = Listing::unordered(['foo', 'bar']);
+
+        $this->assertSame($data['isOrdered'], $listing['isOrdered']);
+        $this->assertSame($data['items'], $listing['items']);
+        $this->assertSame($data, $listing->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function it_must_have_a_valid_prefix()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Listing::unordered(['foo'], 'bar');
+    }
+
+    /**
+     * @test
+     */
+    public function it_must_have_items()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Listing::unordered([]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_must_have_string_items()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Listing::unordered([$this]);
+    }
+
+    public function viewModelProvider() : array
+    {
+        return [
+            'ordered' => [Listing::ordered(['foo', 'bar'], 'roman-upper')],
+            'unordered' => [Listing::unordered(['foo', 'bar'], 'bullet')],
+            'no prefix' => [Listing::unordered(['foo', 'bar'])],
+        ];
+    }
+
+    protected function expectedTemplate() : string
+    {
+        return '/elife/patterns/templates/list.mustache';
+    }
+}
