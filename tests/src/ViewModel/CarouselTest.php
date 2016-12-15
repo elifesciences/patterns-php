7f65cc6d521f466f2063c313292bf2066193d616
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-12-15 07:51:45 +0000

    ELPP-1252 Add carousel (#187)
    * Add carousel
    
    * Match updates

---
+++ tests/src/ViewModel/CarouselTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace tests\eLife\Patterns\ViewModel;
+
+use eLife\Patterns\ViewModel\BackgroundImage;
+use eLife\Patterns\ViewModel\Carousel;
+use eLife\Patterns\ViewModel\CarouselItem;
+use eLife\Patterns\ViewModel\Link;
+use eLife\Patterns\ViewModel\Meta;
+use InvalidArgumentException;
+
+final class CarouselTest extends ViewModelTest
+{
+    /**
+     * @test
+     */
+    public function it_has_data()
+    {
+        $data = [
+            'items' => [
+                [
+                    'subjects' => [
+                        'list' => [
+                            [
+                                'name' => 'subject',
+                                'url' => 'subject-url',
+                            ],
+                        ],
+                    ],
+                    'name' => 'carousel item',
+                    'url' => 'carousel-item-url',
+                    'button' => [
+                        'classes' => 'button--outline',
+                        'path' => 'carousel-item-url',
+                        'text' => 'button',
+                    ],
+                    'meta' => [
+                        'text' => 'meta',
+                    ],
+                    'backgroundImage' => [
+                        'lowResImageSource' => 'lores.jpg',
+                        'highResImageSource' => 'hires.jpg',
+                    ],
+                ],
+            ],
+        ];
+
+        $carouselItem = new CarouselItem([new Link('subject', 'subject-url')], new Link('carousel item', 'carousel-item-url'), 'button', Meta::withText('meta'), new BackgroundImage('lores.jpg', 'hires.jpg'));
+        $carousel = new Carousel($carouselItem);
+
+        $this->assertSame($data, $carousel->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function it_must_have_an_item()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new Carousel();
+    }
+
+    public function viewModelProvider() : array
+    {
+        return [
+            [new Carousel(new CarouselItem([new Link('subject', 'subject-url')], new Link('carousel item', 'carousel-item-url'), 'button', Meta::withText('meta'), new BackgroundImage('lores.jpg', 'hires.jpg')))],
+        ];
+    }
+
+    protected function expectedTemplate() : string
+    {
+        return '/elife/patterns/templates/carousel.mustache';
+    }
+}
