7ec26561604c4a23f5868ea7eb02cd375e9bacef
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-01-18 15:00:22 +0000

    Make search box part of the site header

--- src/ViewModel/SiteHeader.php
+++ src/ViewModel/SiteHeader.php
@@ -18,14 +18,16 @@ final class SiteHeader implements ViewModel
     private $homePagePath;
     private $primaryLinks;
     private $secondaryLinks;
+    private $searchBox;
 
-    public function __construct(string $homePagePath, SiteHeaderNavBar $primaryLinks, SiteHeaderNavBar $secondaryLinks)
+    public function __construct(string $homePagePath, SiteHeaderNavBar $primaryLinks, SiteHeaderNavBar $secondaryLinks, SearchBox $searchBox = null)
     {
         Assertion::notBlank($homePagePath);
 
         $this->homePagePath = $homePagePath;
         $this->primaryLinks = $primaryLinks;
         $this->secondaryLinks = $secondaryLinks;
+        $this->searchBox = $searchBox;
     }
 
     protected function getLocalStyleSheets() : Traversable
@@ -37,6 +39,7 @@ final class SiteHeader implements ViewModel
     {
         yield $this->primaryLinks;
         yield $this->secondaryLinks;
+        yield $this->searchBox;
     }
 
     public function getTemplateName() : string
