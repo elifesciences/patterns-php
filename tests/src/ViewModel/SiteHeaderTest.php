62e914d9bfc5e13abc723d334547d8a4e5036951
Chris Wilkinson <chriswilkinson84@gmail.com>
2017-01-18 15:02:00 +0000

    Search box update (#206)
    * Remove autocomplete from search box
    
    * Make search box part of the site header

--- tests/src/ViewModel/SiteHeaderTest.php
+++ tests/src/ViewModel/SiteHeaderTest.php
@@ -3,10 +3,14 @@
 namespace tests\eLife\Patterns\ViewModel;
 
 use eLife\Patterns\ViewModel\Button;
+use eLife\Patterns\ViewModel\CompactForm;
+use eLife\Patterns\ViewModel\Form;
 use eLife\Patterns\ViewModel\Image;
+use eLife\Patterns\ViewModel\Input;
 use eLife\Patterns\ViewModel\Link;
 use eLife\Patterns\ViewModel\NavLinkedItem;
 use eLife\Patterns\ViewModel\Picture;
+use eLife\Patterns\ViewModel\SearchBox;
 use eLife\Patterns\ViewModel\SiteHeader;
 use eLife\Patterns\ViewModel\SiteHeaderNavBar;
 use InvalidArgumentException;
@@ -18,30 +22,39 @@ final class SiteHeaderTest extends ViewModelTest
     private $homePagePath;
     private $primaryLinks;
     private $secondaryLinks;
+    private $searchBox;
 
     public function setUp()
     {
         parent::setUp();
         $this->img = new Picture(
-          [
-            ['srcset' => '/path/to/svg'],
-          ],
-          new Image('/path/to/fallback/', [500 => '/path/in/srcset'], 'alt text', [])
+            [
+                ['srcset' => '/path/to/svg'],
+            ],
+            new Image('/path/to/fallback/', [500 => '/path/in/srcset'], 'alt text', [])
         );
         $this->homePagePath = '/home/page/path';
         $this->primaryLinks = SiteHeaderNavBar::primary(
-          [
-            NavLinkedItem::asIcon(new Link('text-first', '/path/first'), $this->img),
-            NavLinkedItem::asLink(new Link('text-first', '/path/first'), false),
-            NavLinkedItem::asLink(new Link('text-second', '/path/second'), true),
-          ]
+            [
+                NavLinkedItem::asIcon(new Link('text-first', '/path/first'), $this->img),
+                NavLinkedItem::asLink(new Link('text-first', '/path/first'), false),
+                NavLinkedItem::asLink(new Link('text-second', '/path/second'), true),
+            ]
         );
 
         $this->secondaryLinks = SiteHeaderNavBar::secondary(
-          [
-            NavLinkedItem::asLink(new Link('text-first', '/path/first'), false),
-            NavLinkedItem::asButton(Button::link('button text', '/button/path')),
-          ]
+            [
+                NavLinkedItem::asLink(new Link('text-first', '/path/first'), false),
+                NavLinkedItem::asButton(Button::link('button text', '/button/path')),
+            ]
+        );
+
+        $this->searchBox = new SearchBox(
+            $compactForm = new CompactForm(
+                new Form('/action', 'id', 'GET'),
+                new Input('label', 'search', 'name', 'value', 'placeholder'),
+                'cta'
+            )
         );
     }
 
@@ -50,10 +63,11 @@ final class SiteHeaderTest extends ViewModelTest
      */
     public function it_has_data()
     {
-        $siteHeader = new SiteHeader($this->homePagePath, $this->primaryLinks, $this->secondaryLinks);
+        $siteHeader = new SiteHeader($this->homePagePath, $this->primaryLinks, $this->secondaryLinks, $this->searchBox);
         $this->assertSame($this->homePagePath, $siteHeader['homePagePath']);
         $this->assertSame($this->primaryLinks, $siteHeader['primaryLinks']);
         $this->assertSame($this->secondaryLinks, $siteHeader['secondaryLinks']);
+        $this->assertSame($this->searchBox, $siteHeader['searchBox']);
     }
 
     /**
@@ -93,23 +107,24 @@ final class SiteHeaderTest extends ViewModelTest
         );
 
         $primaryLinks = SiteHeaderNavBar::primary(
-          [
-            NavLinkedItem::asIcon(new Link('text-first', '/path/first'), $img),
-            NavLinkedItem::asLink(new Link('text-first', '/path/first'), false),
-            NavLinkedItem::asLink(new Link('text-second', '/path/second'), true),
-          ]
+            [
+                NavLinkedItem::asIcon(new Link('text-first', '/path/first'), $img),
+                NavLinkedItem::asLink(new Link('text-first', '/path/first'), false),
+                NavLinkedItem::asLink(new Link('text-second', '/path/second'), true),
+            ]
         );
 
         $secondaryLinks = SiteHeaderNavBar::secondary(
-          [
-            NavLinkedItem::asIcon(new Link('text-first', '/path/first'), $img),
-            NavLinkedItem::asLink(new Link('text-first', '/path/first'), false),
-            NavLinkedItem::asButton(Button::link('button text', '/button/path')),
-          ]
+            [
+                NavLinkedItem::asIcon(new Link('text-first', '/path/first'), $img),
+                NavLinkedItem::asLink(new Link('text-first', '/path/first'), false),
+                NavLinkedItem::asButton(Button::link('button text', '/button/path')),
+            ]
         );
 
         return [
-            [new SiteHeader('/home/page/path', $primaryLinks, $secondaryLinks)],
+            'minimum' => [new SiteHeader('/home/page/path', $primaryLinks, $secondaryLinks)],
+            'complete' => [new SiteHeader('/home/page/path', $primaryLinks, $secondaryLinks, $this->searchBox)],
         ];
     }
 
