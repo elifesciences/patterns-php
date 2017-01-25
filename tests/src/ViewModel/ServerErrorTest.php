95f7faed2922dfa18bf625a8c6db02e854fe27d7
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-01-25 16:01:18 +0000

    Add errors

---
+++ tests/src/ViewModel/ServerErrorTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace tests\eLife\Patterns\ViewModel;
+
+use eLife\Patterns\ViewModel\Button;
+use eLife\Patterns\ViewModel\ServerError;
+
+final class ServerErrorTest extends ViewModelTest
+{
+    /**
+     * @test
+     */
+    public function it_has_data()
+    {
+        $data = [
+            'button' => [
+                'classes' => 'button--default',
+                'path' => '#',
+                'text' => 'button',
+            ],
+        ];
+
+        $serverError = new ServerError(Button::link('button', '#'));
+
+        $this->assertSame($data['button'], $serverError['button']->toArray());
+        $this->assertSame($data, $serverError->toArray());
+    }
+
+    public function viewModelProvider() : array
+    {
+        return [
+            'minimum' => [new ServerError()],
+            'with button' => [new ServerError(Button::link('button', '#'))],
+        ];
+    }
+
+    protected function expectedTemplate() : string
+    {
+        return '/elife/patterns/templates/server-error.mustache';
+    }
+}
