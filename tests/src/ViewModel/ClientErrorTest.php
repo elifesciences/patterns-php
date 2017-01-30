1610f8567529f24034f654fe41640557ce6cc754
Chris Wilkinson <chriswilkinson84@gmail.com>
2017-01-30 10:21:03 +0000

    ELPP-2026 Add errors (#200)
    * Add errors
    
    * Update assets
    
    * Back to master

---
+++ tests/src/ViewModel/ClientErrorTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace tests\eLife\Patterns\ViewModel;
+
+use eLife\Patterns\ViewModel\Button;
+use eLife\Patterns\ViewModel\ClientError;
+
+final class ClientErrorTest extends ViewModelTest
+{
+    /**
+     * @test
+     */
+    public function it_has_data()
+    {
+        $data = [
+            'button' => [
+                'classes' => 'button--default',
+                'path' => '#',
+                'text' => 'button',
+            ],
+        ];
+
+        $clientError = new ClientError(Button::link('button', '#'));
+
+        $this->assertSame($data['button'], $clientError['button']->toArray());
+        $this->assertSame($data, $clientError->toArray());
+    }
+
+    public function viewModelProvider() : array
+    {
+        return [
+            'minimum' => [new ClientError()],
+            'with button' => [new ClientError(Button::link('button', '#'))],
+        ];
+    }
+
+    protected function expectedTemplate() : string
+    {
+        return '/elife/patterns/templates/client-error.mustache';
+    }
+}
