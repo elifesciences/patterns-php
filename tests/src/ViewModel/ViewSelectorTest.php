5bf26bd6bb0ca721d6a04b703741eee20a0ba3db
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-03-13 15:16:20 +0000

    Allow figures to be active in the view selector

--- tests/src/ViewModel/ViewSelectorTest.php
+++ tests/src/ViewModel/ViewSelectorTest.php
@@ -29,16 +29,18 @@ final class ViewSelectorTest extends ViewModelTest
                 ],
             ],
             'figureUrl' => 'figures',
+            'figureIsActive' => true,
             'sideBySideUrl' => 'side-by-side',
         ];
 
         $viewSelector = new ViewSelector($data['articleUrl'], $links = array_map(function ($link) {
             return new Link($link['name'], $link['url']);
-        }, $data['jumpLinks']['links']), $data['figureUrl'], $data['sideBySideUrl']);
+        }, $data['jumpLinks']['links']), $data['figureUrl'], $data['figureIsActive'], $data['sideBySideUrl']);
 
         $this->assertSame($data['articleUrl'], $viewSelector['articleUrl']);
         $this->assertEquals($links, $viewSelector['jumpLinks']['links']);
         $this->assertSame($data['figureUrl'], $viewSelector['figureUrl']);
+        $this->assertSame($data['figureIsActive'], $viewSelector['figureIsActive']);
         $this->assertSame($data['sideBySideUrl'], $viewSelector['sideBySideUrl']);
         $this->assertSameWithoutOrder($data, $viewSelector);
     }
@@ -60,8 +62,7 @@ final class ViewSelectorTest extends ViewModelTest
                 new ViewSelector('article'),
             ],
             'complete' => [
-                new ViewSelector('article', [new Link('some link', '#'), new Link('some link', '#')], 'figures',
-                    'side-by-side'),
+                new ViewSelector('article', [new Link('some link', '#'), new Link('some link', '#')], 'figures', true, 'side-by-side'),
             ],
         ];
     }
