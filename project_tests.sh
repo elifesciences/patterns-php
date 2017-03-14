f3ca3bba50548e7aaea35362519bb995fef5e453
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-03-14 09:14:40 +0000

    Composer version

--- project_tests.sh
+++ project_tests.sh
@@ -2,6 +2,6 @@
 set -e
 
 : "${dependencies:?Need to set dependencies environment variable}"
-if [ "$dependencies" = "lowest" ]; then composer1.0 update --prefer-lowest --no-interaction; else composer1.0 update --no-interaction; fi;
+if [ "$dependencies" = "lowest" ]; then composer update --prefer-lowest --no-interaction; else composer update --no-interaction; fi;
 if [ "$dependencies" = "lowest" ]; then proofreader bin src tests; fi;
 vendor/bin/phpunit --log-junit="build/${dependencies}-phpunit.xml"
