73f5b8e9e86f0b34f1e977d9b9ceb26e9f6e7472
Alfred <alfred-ci@elifesciences.org>
2017-04-12 09:44:21 +0100

    Updated pattern-library to 4bc08a7: Fix video requirements (#292)
    * Updated pattern-library to 4bc08a7: Fix video requirements
    
    * Make poster frame optional

--- tests/src/ViewModel/VideoTest.php
+++ tests/src/ViewModel/VideoTest.php
@@ -34,10 +34,10 @@ final class VideoTest extends ViewModelTest
             'autoplay' => true,
             'loop' => true,
         ];
-        $video = new Video($data['posterFrame'], array_map(function ($source) {
+        $video = new Video(array_map(function ($source) {
             return new MediaSource($source['src'], new MediaType($source['mediaType']['forMachine']),
                 new MediaSourceFallback('fallback', true));
-        }, $data['sources']), $data['autoplay'], $data['loop']);
+        }, $data['sources']), $data['posterFrame'], $data['autoplay'], $data['loop']);
 
         $this->assertSameWithoutOrder($data, $video->toArray());
     }
@@ -48,7 +48,7 @@ final class VideoTest extends ViewModelTest
     public function testCantUseAudioAsVideoSource()
     {
         $this->expectException(InvalidArgumentException::class);
-        new Video('http://some.image.com/test.jpg', [new MediaSource('/file.mp4', new MediaType('audio/mp4'))]);
+        new Video([new MediaSource('/file.mp4', new MediaType('audio/mp4'))]);
     }
 
     /**
@@ -57,17 +57,17 @@ final class VideoTest extends ViewModelTest
     public function testCantUseAudioSource()
     {
         $this->expectException(InvalidArgumentException::class);
-        new Video('http://some.image.com/test.jpg', [new MediaSource('/file.mp3', new MediaType('audio/mp3'))]);
+        new Video([new MediaSource('/file.mp3', new MediaType('audio/mp3'))]);
     }
 
     public function viewModelProvider() : array
     {
         return [
-            [
-                new Video('http://some.image.com/test.jpg', [new MediaSource('/file.mp4', new MediaType('video/mp4'), new MediaSourceFallback('fallback', true))], true, true),
+            'complete' => [
+                new Video([new MediaSource('/file.mp4', new MediaType('video/mp4'), new MediaSourceFallback('fallback', true))], 'http://some.image.com/test.jpg', true, true),
             ],
-            [
-                new Video('http://some.image.com/test.jpg', [new MediaSource('/file.mp4', new MediaType('video/mp4'))]),
+            'minimum' => [
+                new Video([new MediaSource('/file.mp4', new MediaType('video/mp4'))]),
             ],
         ];
     }
