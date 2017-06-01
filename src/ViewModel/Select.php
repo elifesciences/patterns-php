47676c309b234bcc84ecfd3ef6ed3483c4f350f5
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-06-01 14:51:37 +0100

    Update form fields

--- src/ViewModel/Select.php
+++ src/ViewModel/Select.php
@@ -10,6 +10,9 @@ use eLife\Patterns\ViewModel;
 
 final class Select implements ViewModel
 {
+    const STATUS_ERROR = 'error';
+    const STATUS_VALID = 'valid';
+
     use ArrayAccessFromProperties;
     use ArrayFromProperties;
     use SimplifyAssets;
@@ -17,15 +20,33 @@ final class Select implements ViewModel
     private $id;
     private $options;
     private $label;
+    private $name;
+    private $required;
+    private $disabled;
+    private $classNames;
 
-    public function __construct(string $id, array $options, FormLabel $label)
-    {
+    public function __construct(
+        string $id,
+        array $options,
+        FormLabel $label,
+        string $name,
+        bool $required = null,
+        bool $disabled = null,
+        string $status = null
+    ) {
         Assertion::notEmpty($options);
         Assertion::allIsInstanceOf($options, SelectOption::class);
+        Assertion::nullOrChoice($status, [self::STATUS_ERROR, self::STATUS_VALID]);
 
         $this->id = $id;
         $this->options = $options;
         $this->label = $label;
+        $this->name = $name;
+        $this->required = $required;
+        $this->disabled = $disabled;
+        if (!empty($status)) {
+            $this->classNames = "select--$status";
+        }
     }
 
     public function getTemplateName() : string
