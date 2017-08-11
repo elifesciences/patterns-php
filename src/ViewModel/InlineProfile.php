52e11f087dce2846eb28d8e235392dde52bd4cd5
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-08-11 17:02:31 +0100

    Add view model

---
+++ src/ViewModel/InlineProfile.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayAccessFromProperties;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class InlineProfile implements ViewModel
+{
+    use ArrayAccessFromProperties;
+    use ArrayFromProperties;
+    use SimplifyAssets;
+
+    private $image;
+    private $text;
+
+    public function __construct(Picture $image, string $text)
+    {
+        Assertion::notBlank($text);
+
+        $this->image = $image;
+        $this->text = $text;
+    }
+
+    public function getTemplateName() : string
+    {
+        return 'resources/templates/inline-profile.mustache';
+    }
+
+    public function getStyleSheets() : Traversable
+    {
+        yield 'resources/assets/css/inline-profile.css';
+    }
+}
