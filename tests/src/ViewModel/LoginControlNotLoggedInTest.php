f2ab66f7555c0fa2527f94208431b7fc30089a1d
David Moulton <davidcmoulton@users.noreply.github.com>
2017-10-19 10:48:03 +0100

    ELPP-2983 - profile page - login control (#496)
    * Style change implementing login button w mixins on pattern library
    
    * Update from pattern library with revised button style
    
    * Add confirm style to button view model
    
    * Pattern library styles update
    
    * Pattern library styles update
    
    * Update profile login control pattern from pattern library
    
    * Add view model and tests for profile login control pattern
    
    * Add profile login control pattern update
    
    * Add missing
    
    * Update approach to ProfileLoginControl view model
    
    * Code style
    
    * Match case of method name in test
    
    * Tighten method visibility
    
    * Use implode
    
    * Simplify generation of data attributes for profile login control
    
    * Parametise previously hard coded string in profile login control
    
    * Update names
    
    * Update site header test to work with refactored login control
    
    * Code style
    
    * Update test site header test
    
    * Simplify data structure required for LoginControl view model
    
    * Code style
    
    * Update assets
    
    * Supply the login control icon as a Picture argument
    
    * Parametise login control's subsidiary text
    
    * Code style
    
    * Match pattern library
    
    * match pattern library
    
    * Update assertion
    
    * CS and code tweaks

---
+++ tests/src/ViewModel/LoginControlNotLoggedInTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace tests\eLife\Patterns\ViewModel;
+
+use eLife\Patterns\ViewModel\LoginControl;
+use InvalidArgumentException;
+
+final class LoginControlNotLoggedInTest extends ViewModelTest
+{
+    /**
+     * @test
+     */
+    public function it_has_data()
+    {
+        $data = [
+            'button' => [
+                'classes' => 'button--extra-small button--confirm',
+                'path' => 'The uri',
+                'text' => 'Log in / Register',
+            ],
+        ];
+
+        $loginControl = LoginControl::notLoggedIn($data['button']['text'], $data['button']['path']);
+
+        $this->assertSame(null, $loginControl['isLoggedIn']);
+        $this->assertSame($data, $loginControl->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function it_must_be_passed_a_uri()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        LoginControl::notLoggedIn('text', '');
+    }
+
+    /**
+     * @test
+     */
+    public function it_must_be_passed_text()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        LoginControl::notLoggedIn('', '/log-in');
+    }
+
+    /**
+     * @test
+     */
+    public function it_must_indicate_its_not_logged_in()
+    {
+        $loginControl = LoginControl::notLoggedIn('text', 'some uri');
+        $this->assertNull($loginControl['isLoggedIn']);
+    }
+
+    public function viewModelProvider() : array
+    {
+        return [
+            [LoginControl::notLoggedIn('some text', '#loginUri')],
+        ];
+    }
+
+    protected function expectedTemplate() : string
+    {
+        return 'resources/templates/login-control.mustache';
+    }
+}
