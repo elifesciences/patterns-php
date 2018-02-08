2d697370c0935f9dee1485ba69cf1cee107e27a5
Chris Wilkinson <chriswilkinson84@gmail.com>
2018-02-08 15:41:11 +0000

    ELPP-3349 Add PersonalisedCoverDownload (#555)
    * Add Download
    
    * Rename
    
    * Use Paragraph
    
    * Back to master

---
+++ src/ViewModel/PersonalisedCoverDownload.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayAccessFromProperties;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ComposedAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class PersonalisedCoverDownload implements ViewModel
+{
+    use ArrayAccessFromProperties;
+    use ArrayFromProperties;
+    use ComposedAssets;
+
+    private $text;
+    private $buttonCollection;
+
+    public function __construct(array $text, ButtonCollection $buttonCollection)
+    {
+        Assertion::notEmpty($text);
+        Assertion::allIsInstanceOf($text, Paragraph::class);
+
+        if (!$buttonCollection['centered']) {
+            $buttonCollection = FlexibleViewModel::fromViewModel($buttonCollection)
+                ->withProperty('centered', true);
+        }
+
+        $this->text = $text;
+        $this->buttonCollection = $buttonCollection;
+    }
+
+    protected function getLocalStyleSheets() : Traversable
+    {
+        yield 'resources/assets/css/personalised-cover-download.css';
+    }
+
+    protected function getComposedViewModels() : Traversable
+    {
+        yield from $this->text;
+        yield $this->buttonCollection;
+    }
+
+    public function getTemplateName() : string
+    {
+        return 'resources/templates/personalised-cover-download.mustache';
+    }
+}
