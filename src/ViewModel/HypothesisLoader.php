98236b25cce1e0c446854f4e5c6cbed906d140c9
David Moulton <d.moulton@elifesciences.org>
2018-02-23 13:50:50 +0000

    Add HypothesisLoader view model and tests

---
+++ src/ViewModel/HypothesisLoader.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayAccessFromProperties;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+
+final class HypothesisLoader implements ViewModel
+{
+    use ArrayAccessFromProperties;
+    use ArrayFromProperties;
+    use SimplifyAssets;
+
+    private $usernameUrl;
+    private $hypothesisApiUrl;
+    private $hypothesisAuthority;
+    private $iconPath;
+    private $loginPath;
+    private $profilePath;
+    private $logoutPath;
+    private $grantToken;
+
+    private function __construct(
+        string $usernameUrl,
+        string $hypothesisApiUrl,
+        string $hypothesisAuthority,
+        string $iconPath,
+        string $loginPath = null,
+        string $profilePath = null,
+        string $logoutPath = null,
+        string $grantToken = null
+    ) {
+        Assertion::notBlank($usernameUrl);
+        Assertion::notBlank($hypothesisApiUrl);
+        Assertion::notBlank($hypothesisAuthority);
+        Assertion::notBlank($iconPath);
+
+        $this->usernameUrl = $usernameUrl;
+        $this->hypothesisApiUrl = $hypothesisApiUrl;
+        $this->hypothesisAuthority = $hypothesisAuthority;
+        $this->iconPath = $iconPath;
+        $this->loginPath = $loginPath;
+        $this->profilePath = $profilePath;
+        $this->logoutPath = $logoutPath;
+        $this->grantToken = $grantToken;
+    }
+
+    public static function loggedOut(
+        string $usernameUrl,
+        string $hypothesisApiUrl,
+        string $hypothesisAuthority,
+        string $iconPath,
+        string $loginPath,
+        string $grantToken = null
+    ) {
+        Assertion::notBlank($loginPath);
+
+        return new static($usernameUrl, $hypothesisApiUrl, $hypothesisAuthority, $iconPath, $loginPath, null, null, $grantToken);
+    }
+
+    public static function loggedIn(
+        string $usernameUrl,
+        string $hypothesisApiUrl,
+        string $hypothesisAuthority,
+        string $iconPath,
+        string $profilePath,
+        string $logoutPath,
+        string $grantToken = null
+    ) {
+        Assertion::notBlank($profilePath);
+        Assertion::notBlank($logoutPath);
+
+        return new static($usernameUrl, $hypothesisApiUrl, $hypothesisAuthority, $iconPath, null, $profilePath, $logoutPath, $grantToken);
+    }
+
+    public function getTemplateName() : string
+    {
+        return 'resources/templates/hypothesis-loader.mustache';
+    }
+
+}
---
+++ tests/src/ViewModel/HypothesisLoaderTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace tests\eLife\Patterns\ViewModel;
+
+use eLife\Patterns\ViewModel\HypothesisLoader;
+
+final class HypothesisLoaderTest extends ViewModelTest
+{
+    /**
+     * @test
+     */
+    public function it_has_data()
+    {
+        $data = [
+            'core' => [
+                'usernameUrl' => 'https://example.com/username',
+                'hypothesisApiUrl' => 'https://example.com/h-api',
+                'hypothesisAuthority' => 'Cartman',
+                'iconPath' => 'https://example.com/icon',
+                'grantToken' => 'someGrantToken'
+            ],
+
+            'loggedIn' => [
+                'profilePath' => 'https://example.com/profile',
+                'logoutPath' => 'https://example.com/log-out',
+            ],
+
+            'loggedOut' => [
+                'loginPath' => 'https://example.com/log-in',
+            ],
+        ];
+
+        $loaderLoggedIn = HypothesisLoader::loggedIn(
+            $data['core']['usernameUrl'],
+            $data['core']['hypothesisApiUrl'],
+            $data['core']['hypothesisAuthority'],
+            $data['core']['iconPath'],
+            $data['loggedIn']['profilePath'],
+            $data['loggedIn']['logoutPath'],
+            $data['core']['grantToken']
+        );
+
+        $this->assertSame($data['core']['usernameUrl'], $loaderLoggedIn['usernameUrl']);
+        $this->assertSame($data['core']['hypothesisApiUrl'], $loaderLoggedIn['hypothesisApiUrl']);
+        $this->assertSame($data['core']['hypothesisAuthority'], $loaderLoggedIn['hypothesisAuthority']);
+        $this->assertSame($data['core']['iconPath'], $loaderLoggedIn['iconPath']);
+        $this->assertSame($data['loggedIn']['profilePath'], $loaderLoggedIn['profilePath']);
+        $this->assertSame($data['loggedIn']['logoutPath'], $loaderLoggedIn['logoutPath']);
+        $this->assertSame($data['core']['grantToken'], $loaderLoggedIn['grantToken']);
+
+        $this->assertSameWithoutOrder(array_merge($data['core'], $data['loggedIn']), $loaderLoggedIn->toArray());
+
+        $loaderLoggedOut = HypothesisLoader::loggedOut(
+            $data['core']['usernameUrl'],
+            $data['core']['hypothesisApiUrl'],
+            $data['core']['hypothesisAuthority'],
+            $data['core']['iconPath'],
+            $data['loggedOut']['loginPath'],
+            $data['core']['grantToken']
+        );
+
+        $this->assertSame($data['core']['usernameUrl'], $loaderLoggedOut['usernameUrl']);
+        $this->assertSame($data['core']['hypothesisApiUrl'], $loaderLoggedOut['hypothesisApiUrl']);
+        $this->assertSame($data['core']['hypothesisAuthority'], $loaderLoggedOut['hypothesisAuthority']);
+        $this->assertSame($data['core']['iconPath'], $loaderLoggedOut['iconPath']);
+        $this->assertSame($data['loggedOut']['loginPath'], $loaderLoggedOut['loginPath']);
+        $this->assertSame($data['core']['grantToken'], $loaderLoggedOut['grantToken']);
+
+        $this->assertSameWithoutOrder(array_merge($data['core'], $data['loggedOut']), $loaderLoggedOut->toArray());
+    }
+
+    public function viewModelProvider() : array
+    {
+        return [
+            'logged-out' => [
+                HypothesisLoader::loggedOut('https://usernameUrl', 'https://hypothesisApiUrl', 'hypothesisAuthority', 'https://iconPath', 'loginPath', 'grantToken'),
+            ],
+
+            'no-grant-token' => [
+                HypothesisLoader::loggedOut('https://usernameUrl', 'https://hypothesisApiUrl', 'hypothesisAuthority', 'https://iconPath','loginPath', null),
+            ],
+
+            'logged-in' => [
+                HypothesisLoader::loggedIn('https://usernameUrl', 'https://hypothesisApiUrl', 'hypothesisAuthority', 'https://iconPath', 'profilePath', 'logoutPath', 'grantToken'),
+            ],
+
+        ];
+    }
+
+    protected function expectedTemplate() : string
+    {
+        return 'resources/templates/hypothesis-loader.mustache';
+    }
+}
