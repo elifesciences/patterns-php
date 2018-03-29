0bb7169c69d40e8550aa0b9c622ab4c4707b2475
David Moulton <d.moulton@elifesciences.org>
2018-03-29 12:17:23 +0100

    Add test for twitterUrl

--- tests/src/ViewModel/SocialMediaSharersTest.php
+++ tests/src/ViewModel/SocialMediaSharersTest.php
@@ -36,6 +36,59 @@ final class SocialMediaSharersTest extends ViewModelTest
     /**
      * @test
      */
+    public function its_twitterUrl_parameter_values_are_correctly_formed()
+    {
+        // One character shorter than theoretical max to account for space injected by Twitter between the text and url
+        $overallMaxLength = 139;
+
+        $tweets = [
+            [
+                'url' => 'https://elifesciences.org/articles/34901',
+                'title' => 'Retrotransposons: On the move',
+            ],
+            [
+                'url' => 'https://elifesciences.org/labs/0af6527f/building-a-pattern-library-for-scholarly-publishing',
+                'title' => 'Building a pattern library for scholarly publishing',
+            ],
+            [
+                'url' => 'https://elifesciences.org/labs/0a677ba3/prototyping-tools-to-improve-the-understanding-of-science',
+                'title' => 'Prototyping tools to improve the understanding of science',
+            ],
+            [
+                'url' => 'https://elifesciences.org/inside-elife/912b0679/early-career-advisory-group-elife-welcomes-150-ambassadors-of-good-practice-in-science',
+                'title' => 'Early-Career Advisory Group: eLife welcomes 150 Ambassadors of good practice in science',
+            ],
+        ];
+
+        foreach ($tweets as $tweet) {
+
+            $url = $tweet['url'];
+            $title = $tweet['title'];
+
+            $twitterUrl = (new SocialMediaSharers($title, $url))['twitterUrl'];
+
+            preg_match('/url=([^&]+)/', $twitterUrl,$urlMatch);
+            $decodedUrl = urldecode($urlMatch[1]);
+            $this->assertEquals($url, $decodedUrl);
+
+            preg_match('/text=([^&]+)/', $twitterUrl,$textMatch);
+            $decodedText = urldecode($textMatch[1]);
+            if ($title === $decodedText) {
+                $this->assertLessThanOrEqual($overallMaxLength, strlen($decodedText . $decodedUrl));
+            } else {
+                // The title has been truncated to stop the overall tweet being too long
+                $ellipsis = ' &#8230;';
+                // -1 to account for the display of the ellipsis
+                $charCountTruncatedTitle = $overallMaxLength - strlen($decodedUrl) - 1;
+                $this->assertEquals(substr($title, 0, $charCountTruncatedTitle) . $ellipsis, $decodedText);
+                $this->assertLessThanOrEqual($overallMaxLength + strlen($ellipsis) - 1, strlen($decodedText));
+            }
+        }
+    }
+
+    /**
+     * @test
+     */
     public function it_must_be_given_a_title()
     {
         $this->expectException(InvalidArgumentException::class);
