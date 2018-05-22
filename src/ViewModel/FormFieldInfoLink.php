be328d1b4bcea651013c7f3a6afe68b9f013ef57
David Moulton <d.moulton@elifesciences.org>
2018-05-22 10:45:11 +0100

    Add FormFieldInfoLink view model

---
+++ src/ViewModel/FormFieldInfoLink.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayAccessFromProperties;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+use Traversable;
+
+final class FormFieldInfoLink implements ViewModel
+{
+    use ArrayAccessFromProperties;
+    use ArrayFromProperties;
+    use SimplifyAssets;
+
+    private $text;
+    private $url;
+    private $alignLeft;
+
+    private function __construct(
+        string $text,
+        string $url,
+        bool $alignLeft = false
+    ) {
+        Assertion::notBlank($text);
+        Assertion::notBlank($url);
+
+        $this->text = $text;
+        $this->url = $url;
+        if ($alignLeft) {
+            $this->alignLeft = $alignLeft;
+        }
+    }
+
+    public static function alignedLeft(string $text, string $url)
+    {
+        return new static($text, $url, true);
+    }
+
+    public static function alignedRight(string $text, string $url)
+    {
+        return new static($text, $url);
+    }
+
+    public function getLocalStyleSheets() : Traversable
+    {
+        yield 'resources/assets/css/form-field-info-link.scss';
+    }
+
+    public function getTemplateName() : string
+    {
+        return 'resources/templates/form-field-info-link.mustache';
+    }
+}
---
+++ tests/src/ViewModel/FormFieldInfoLinkTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace tests\eLife\Patterns\ViewModel;
+
+use eLife\Patterns\ViewModel\FormFieldInfoLink;
+
+final class FormFieldInfoLinkTest extends ViewModelTest
+{
+    /**
+     * @test
+     */
+    public function it_has_data()
+    {
+        $data = [
+            'text' => 'some text',
+            'url' => 'http://example.com',
+            'alignLeft' => true,
+        ];
+        $formFieldInfoLink = FormFieldInfoLink::alignedLeft($data['text'], $data['url']);
+
+        $this->assertSame($data['text'], $formFieldInfoLink['text']);
+        $this->assertSame($data['url'], $formFieldInfoLink['url']);
+        $this->assertSame($data['alignLeft'], $formFieldInfoLink['alignLeft']);
+
+        $this->assertSame($data, $formFieldInfoLink->toArray());
+    }
+
+    public function it_can_be_aligned_right() {
+        $formFieldInfoLink = FormFieldInfoLink::alignedRight('text', 'https://url');
+        $this->assertFalse($formFieldInfoLink['alignLeft']);
+    }
+
+    public function viewModelProvider() : array
+    {
+        return [
+            'alignedLeft' => [FormFieldInfoLink::alignedLeft('text', 'https://url')],
+            'alignedRight' => [FormFieldInfoLink::alignedRight('text', 'https://url')],
+        ];
+    }
+
+    protected function expectedTemplate() : string
+    {
+        return 'resources/templates/form-field-info-link.mustache';
+    }
+}
