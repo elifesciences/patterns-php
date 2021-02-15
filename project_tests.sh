123937b73692b8c83d1dd7c8786a298d1317a372
nlisgo <nathan@aeopus.com>
2021-02-15 23:18:34 +0000

    Replace proofreader with phpcs

--- project_tests.sh
+++ project_tests.sh
@@ -3,6 +3,6 @@ set -e
 
 : "${dependencies:?Need to set dependencies environment variable}"
 if [ "$dependencies" = "lowest" ]; then composer update --prefer-lowest --no-interaction; else composer update --no-interaction; fi;
-if [ "$dependencies" = "lowest" ]; then proofreader bin src; fi;
-if [ "$dependencies" = "lowest" ]; then proofreader --no-phpcpd tests; fi;
+if [ "$dependencies" = "lowest" ]; then vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p bin src; fi;
+if [ "$dependencies" = "lowest" ]; then vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p tests; fi;
 vendor/bin/phpunit --log-junit="build/${dependencies}-phpunit.xml"
