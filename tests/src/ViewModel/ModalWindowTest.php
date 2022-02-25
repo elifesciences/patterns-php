e68dc87e6018d62374e84b8f5aceeba720f8da90
Nathan Lisgo <nathan@aeopus.com>
2022-02-25 18:06:04 +0000

    Add support for triggerId in ModalWindow (#838)

--- tests/src/ViewModel/ModalWindowTest.php
+++ tests/src/ViewModel/ModalWindowTest.php
@@ -16,25 +16,19 @@ final class ModalWindowTest extends ViewModelTest
             'title' => 'Here is a title',
             'body' => 'Here is some text for the body section.',
             'closeBtnText' => 'Close',
+            'triggerId' => 'trigger-id',
         ];
-        $modalWindow = ModalWindow::create('Here is a title', 'Here is some text for the body section.', 'Close');
+        $modalWindow = ModalWindow::create(
+            'Here is a title',
+            'Here is some text for the body section.',
+            'Close',
+            'trigger-id'
+        );
 
         $this->assertSame($data['title'], $modalWindow['title']);
         $this->assertSame($data['body'], $modalWindow['body']);
         $this->assertSame($data['closeBtnText'], $modalWindow['closeBtnText']);
-    }
-
-    /**
-     * @test
-     */
-    public function it_may_have_close_button_text()
-    {
-        $with = ModalWindow::create('title', 'body', 'closeBtnText');
-        $without = ModalWindow::create('title', 'body');
-
-        $this->assertArrayHasKey('closeBtnText', $with->toArray());
-
-        $this->assertArrayNotHasKey('closeBtnText', $without->toArray());
+        $this->assertSame($data['triggerId'], $modalWindow['triggerId']);
     }
 
     /**
@@ -57,13 +51,39 @@ final class ModalWindowTest extends ViewModelTest
         ModalWindow::create('title', '');
     }
 
+    /**
+     * @test
+     */
+    public function it_may_have_close_button_text()
+    {
+        $with = ModalWindow::create('title', 'body', 'closeBtnText');
+        $without = ModalWindow::create('title', 'body');
+
+        $this->assertArrayHasKey('closeBtnText', $with->toArray());
+
+        $this->assertArrayNotHasKey('closeBtnText', $without->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function it_may_have_a_trigger_id()
+    {
+        $with = ModalWindow::create('title', 'body', null, 'trigger-id');
+        $without = ModalWindow::create('title', 'body');
+
+        $this->assertArrayHasKey('triggerId', $with->toArray());
+
+        $this->assertArrayNotHasKey('triggerId', $without->toArray());
+    }
+
     public function viewModelProvider() : array
     {
         return [
             'minimal' => [ModalWindow::create('title', 'body')],
-            'complete' => [ModalWindow::create('title', 'body', 'closeBtnText')],
+            'complete' => [ModalWindow::create('title', 'body', 'closeBtnText', 'trigger-id')],
             'small minimal' => [ModalWindow::small('title', 'body')],
-            'small complete' => [ModalWindow::small('title', 'body', 'closeBtnText')],
+            'small complete' => [ModalWindow::small('title', 'body', 'closeBtnText', 'trigger-id')],
         ];
     }
 
