2dcb9a329dcfe10feba4afa253810c8889f0c548
nlisgo <nathan@aeopus.com>
2022-03-14 21:45:01 +0000

    Allow doi without a link

--- tests/src/ViewModel/DoiTest.php
+++ tests/src/ViewModel/DoiTest.php
@@ -21,6 +21,9 @@ final class DoiTest extends ViewModelTest
         $this->assertSame($data, $doi->toArray());
     }
 
+    /**
+     * @test
+     */
     public function it_must_be_a_doi()
     {
         $this->expectException(InvalidArgumentException::class);
@@ -28,10 +31,25 @@ final class DoiTest extends ViewModelTest
         new Doi('foo');
     }
 
+    /**
+     * @test
+     */
+    public function it_may_not_have_a_link()
+    {
+        $with = new Doi('10.7554/eLife.00001', true);
+        $without = new Doi('10.7554/eLife.00001');
+
+        $this->assertArrayHasKey('doiWithoutLink', $with->toArray());
+        $this->assertTrue($with->toArray()['doiWithoutLink']);
+
+        $this->assertArrayNotHasKey('doiWithoutLink', $without->toArray());
+    }
+
     public function viewModelProvider() : array
     {
         return [
-            [new Doi('10.7554/eLife.10181.001')],
+            ['minimum' => new Doi('10.7554/eLife.10181.001')],
+            ['complete' => new Doi('10.7554/eLife.10181.001', true)],
         ];
     }
 
