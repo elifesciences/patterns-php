8964ddf7d34ea054acf4dbd88bbae4fec5c3f542
Alfred <alfred-ci@elifesciences.org>
2022-04-20 18:52:47 +0930

    Updated pattern-library to 0624693: Allow PMC check before displaying ReadCube link (#849)
    * Add support for PMC check before displaying ReadCube link
    
    * Updated pattern-library to 0624693: Allow PMC check before displaying ReadCube link
    
    Co-authored-by: nlisgo <nathan@aeopus.com>

--- tests/src/ViewModel/ArticleDownloadLinkTest.php
+++ tests/src/ViewModel/ArticleDownloadLinkTest.php
@@ -31,15 +31,37 @@ final class ArticleDownloadLinkTest extends PHPUnit_Framework_TestCase
             'secondary' => [
                 'name' => 'secondary name', 'url' => 'secondary url',
             ],
+            'checkPMC' => 'https://checkpmc.example',
         ];
 
         $articleDownloadLink = new ArticleDownloadLink(
             new Link('primary name', 'primary url'),
-            new Link('secondary name', 'secondary url')
+            new Link('secondary name', 'secondary url'),
+            'https://checkpmc.example'
         );
 
         $this->assertSame($data['primary'], $articleDownloadLink['primary']->toArray());
         $this->assertSame($data['secondary'], $articleDownloadLink['secondary']->toArray());
+        $this->assertSame($data['checkPMC'], $articleDownloadLink['checkPMC']);
         $this->assertSame($data, $articleDownloadLink->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function it_may_have_a_check_pmc_url()
+    {
+        $with = new ArticleDownloadLink(
+            new Link('primary name', 'primary url'),
+            null,
+            'https://checkpmc.example'
+        );
+        $without = new ArticleDownloadLink(
+            new Link('primary name', 'primary url')
+        );
+
+        $this->assertArrayHasKey('checkPMC', $with->toArray());
+
+        $this->assertArrayNotHasKey('checkPMC', $without->toArray());
     }
 }
