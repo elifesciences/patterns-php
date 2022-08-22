2183353e8885ba8fee44dd6ce3ad4569a27c869b
SaMoghadamzadeh <saeed.moghadamzadeh1@endava.com>
2022-08-22 10:23:49 +0200

    Fix project_tests.sh

--- project_tests.sh
+++ project_tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 : "${dependencies:?Need to set dependencies environment variable}"
-if [ "$dependencies" = "lowest" ]; then composer1 update --prefer-lowest --no-interaction; else composer1 update --no-interaction; fi;
+if [ "$dependencies" = "lowest" ]; then composer update --prefer-lowest --no-interaction; else composer update --no-interaction; fi;
 if [ "$dependencies" = "lowest" ]; then vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p bin src; fi;
 if [ "$dependencies" = "lowest" ]; then vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p tests; fi;
 vendor/bin/phpunit --log-junit="build/${dependencies}-phpunit.xml"
