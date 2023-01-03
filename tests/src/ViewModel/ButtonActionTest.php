4c4cc75666a2c914d238b570016557585e2f50be
Cosmin-Daniel <107858401+Cosmin-Daniel@users.noreply.github.com>
2023-01-03 16:43:51 +0200

    Add Button Action (citation, comment, download, share) (#926)
    * add Button Action
    
    * fix test assertion
    
    * fix tests
    
    * try to fix tests with new commit
    
    * add test for invalid variant
    
    * use constants instead of strings
    add default class 'button--action'
    
    * add path as required and id as optional

---
+++ tests/src/ViewModel/ButtonActionTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace tests\eLife\Patterns\ViewModel;
+
+use eLife\Patterns\ViewModel\Button;
+use InvalidArgumentException;
+
+final class ButtonActionTest extends ViewModelTest
+{
+    /**
+     * @test
+     */
+    public function it_has_data()
+    {
+        $data = [
+            'text' => 'Button action (citation)',
+            'path' => '#citation',
+            'id' => 'button-action-citation',
+            'classes' => 'button--default button--action icon icon-citation',
+        ];
+        $buttonAction = Button::action($data['text'], $data['path'], 1, $data['id'], 'citation');
+        $this->assertSameWithoutOrder($data, $buttonAction->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function it_cannot_have_an_invalid_variant()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Button::action('text', "#", 1, null, 'foo');
+    }
+
+    public function viewModelProvider() : array
+    {
+        return [
+            'basic' => [Button::action('Button action', '#')],
+            'citation' => [Button::action('Button action (citation)', "#", 1, null, Button::ACTION_VARIANT_CITATION)],
+            'comment' => [Button::action('Button action (comment)', "#", 1, null, Button::ACTION_VARIANT_COMMENT)],
+            'download' => [Button::action('Button action (download)', "#", 1, null, Button::ACTION_VARIANT_DOWNLOAD)],
+            'share' => [Button::action('Button action (share)', "#", 1, null, Button::ACTION_VARIANT_SHARE)],
+        ];
+    }
+
+    protected function expectedTemplate() : string
+    {
+        return 'resources/templates/button.mustache';
+    }
+}
