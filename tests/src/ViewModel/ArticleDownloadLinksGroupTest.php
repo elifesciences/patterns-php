91d85dceffcc5707afdc39b340dabfebdd20d054
viktoria-mahmud <111341278+viktoria-mahmud@users.noreply.github.com>
2023-05-12 12:58:52 +0300

    Update Article download link (#999)

--- tests/src/ViewModel/ArticleDownloadLinksGroupTest.php
+++ tests/src/ViewModel/ArticleDownloadLinksGroupTest.php
@@ -40,20 +40,22 @@ final class ArticleDownloadLinksGroupTest extends PHPUnit_Framework_TestCase
             'items' => [
                 [
                     'primary' => [
-                        'name' => 'primary name',
-                        'url' => 'primary url',
-                        'attributes' => [
-                            [
-                                'key' => 'key',
-                                'value' => 'value',
+                       'link' => [
+                            'name' => 'primary name',
+                            'url' => 'primary url',
+                            'attributes' => [
+                                [
+                                    'key' => 'key',
+                                    'value' => 'value',
+                                ],
                             ],
                         ],
+                        'checkPMC' => 'https://checkpmc.example'
                     ],
                     'secondary' => [
                         'name' => 'secondary name',
                         'url' => 'secondary url',
-                    ],
-                    'checkPMC' => 'https://checkpmc.example',
+                    ]
                 ],
             ],
             'intro' => 'intro text',
