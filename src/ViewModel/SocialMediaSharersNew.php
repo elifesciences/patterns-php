16a59c6d747abb7d7a37aa17662928b38bf243e9
Viktoria Nedyalkova <viktoria.nedyalkova@endava.com>
2023-08-10 12:55:02 +0300

    Update Twitter text

--- src/ViewModel/SocialMediaSharersNew.php
+++ src/ViewModel/SocialMediaSharersNew.php
@@ -20,20 +20,26 @@ final class SocialMediaSharersNew implements ViewModel
     private $redditUrl;
     private $mastodonUrl;
 
-    public function __construct(string $title, string $url, $includeEmail = true, bool $hasMastodon = false, bool $hasComment = false)
+    public function __construct(string $title, string $url, $includeEmail = true, bool $hasMastodon = false, bool $hasComment = false, bool $hasUpdatedTwitterText = false)
     {
         Assertion::notBlank($title);
         Assertion::url($url);
 
         $encodedTitle = rawurlencode($title);
+        $encodedTwitterTitle = "In%20%40eLife%3A%20" . $encodedTitle;
         $encodedUrl = rawurlencode($url);
 
         if ($includeEmail) {
             $this->emailUrl = "mailto:?subject={$encodedTitle}&body={$encodedUrl}";
         }
 
+        if ($hasUpdatedTwitterText) {
+            $this->twitterUrl = "https://twitter.com/intent/tweet/?text={$encodedTwitterTitle}&url={$encodedUrl}";;
+        } else {
+            $this->twitterUrl = "https://twitter.com/intent/tweet/?text={$encodedTitle}&url={$encodedUrl}";
+        }
+
         $this->facebookUrl = "https://facebook.com/sharer/sharer.php?u={$encodedUrl}";
-        $this->twitterUrl = "https://twitter.com/intent/tweet/?text={$encodedTitle}&url={$encodedUrl}";
         $this->linkedInUrl = "https://www.linkedin.com/shareArticle?title={$encodedTitle}&url={$encodedUrl}";
         $this->redditUrl = "https://reddit.com/submit/?title={$encodedTitle}&url={$encodedUrl}";
         if ($hasMastodon) {
