f7251ac82e8276d58d203c1b95bae3fe1b7a6d5f
viktoria-mahmud <111341278+viktoria-mahmud@users.noreply.github.com>
2024-10-23 16:29:37 +0300

    Update meta component (#1044)
    * Update meta component
    
    * Update statuses
    
    * Merge develop
    
    * Update meta content
    
    * Update styles and meta component
    
    * Update Meta
    
    * Update Meta component
    
    * Update meta
    
    * Update with latest version
    
    * Update style
    
    * Update styles
    
    * Update resources
    
    * Merge Previous version warning logic
    
    * update from patterns
    
    * Revert "update from patterns"
    
    This reverts commit 4dd13dbd0a389c9617ad0a1e01b19a1c00eb3b06.
    
    * Reapply "update from patterns"
    
    This reverts commit dc385468cc0d03d173198a15e54d67be4ae91fbe.
    
    ---------
    
    Co-authored-by: Dave <discodavey@gmail.com>
    Co-authored-by: Giorgio Sironi <g.sironi@elifesciences.org>

--- src/ViewModel/Meta.php
+++ src/ViewModel/Meta.php
@@ -9,27 +9,47 @@ use eLife\Patterns\ViewModel;
 
 final class Meta implements ViewModel
 {
+    const STATUS_NOT_REVISED = 'Not yet revised';
+    const STATUS_REVISED = 'Revised';
+    const STATUS_VOR = 'Version of Record';
+    const STATUS_POA = 'Accepted Manuscript';
+
+    const COLOR_NOT_REVISED = 'not-revised';
+    const COLOR_REVISED = 'revised';
+    const COLOR_VOR = 'vor';
+
     use ArrayAccessFromProperties;
     use ArrayFromProperties;
 
     private $url;
     private $text;
     private $date;
+    private $articleStatus;
+    private $articleStatusColorClass;
+    private $version;
 
-    private function __construct(string $url = null, string $text = null, Date $date = null)
+    private function __construct(string $url = null, string $text = null, Date $date = null, string $articleStatus = null,
+    string $articleStatusColorClass = null, string $version = null)
     {
         if ($date instanceof Date) {
             Assertion::false($date['isExpanded']);
         }
 
+        Assertion::nullOrChoice($articleStatus, [self::STATUS_NOT_REVISED, self::STATUS_REVISED, self::STATUS_VOR, self::STATUS_POA]);
+        Assertion::nullOrChoice($articleStatusColorClass, [self::COLOR_NOT_REVISED, self::COLOR_REVISED, self::COLOR_VOR]);
+
         $this->url = $url ?? false;
         $this->text = $text;
         $this->date = $date;
+        $this->articleStatus = $articleStatus;
+        $this->articleStatusColorClass = $articleStatusColorClass;
+        $this->version = $version;
     }
 
-    public static function withLink(Link $link, Date $date = null) : Meta
+    public static function withLink(Link $link, Date $date = null, string $articleStatus = null,
+    string $articleStatusColorClass = null, string $version = null) : Meta
     {
-        return new self($link['url'], $link['name'], $date);
+        return new self($link['url'], $link['name'], $date, $articleStatus, $articleStatusColorClass, $version);
     }
 
     public static function withText(string $text, Date $date = null) : Meta
