17997c1c1bb7160ab1bb29b56fbf901c95d06c65
David Moulton <d.moulton@elifesciences.org>
2024-11-20 13:47:17 +0000

    Continue expansion step from pattern-library to support a rename
    https://github.com/elifesciences/issues/issues/9034

---
+++ src/ViewModel/AssessmentTermsArticle.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayAccessFromProperties;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ViewModel;
+
+final class AssessmentTermsArticle implements ViewModel
+{
+    use ArrayAccessFromProperties;
+    use ArrayFromProperties;
+
+    private $title;
+    private $termDescription;
+    private $terms;
+    private $termDescriptionAriaLabel;
+
+    public function __construct(string $title, string $termDescription, array $terms, string $termDescriptionAriaLabel = null)
+    {
+        Assertion::notBlank($title);
+        Assertion::notBlank($termDescription);
+        Assertion::notEmpty($terms);
+        Assertion::nullOrNotBlank($termDescriptionAriaLabel);
+
+        $this->title = $title;
+        $this->termDescription = $termDescription;
+        $this->termDescriptionAriaLabel = $termDescriptionAriaLabel;
+
+        $this->terms = array_map(function ($term) {
+            $result = ['term' => $term['term']];
+
+            if (isset($term['isHighlighted']) && $term['isHighlighted']) {
+                $result['isHighlighted'] = true;
+            }
+
+            return $result;
+        }, $terms);
+    }
+
+    public function getTemplateName() : string
+    {
+        return 'resources/templates/assessment-terms-article.mustache';
+    }
+}
