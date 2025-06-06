13fab3b1923021d3e351e70d4058aea4a6cf4f0b
LinaKind <l.kinduryte@elifesciences.org>
2025-06-06 16:50:59 +0300

    Add an Altmetric view model
    https://github.com/elifesciences/journal/issues/1948

---
+++ src/ViewModel/Altmetric.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace eLife\Patterns\ViewModel;
+
+use Assert\Assertion;
+use eLife\Patterns\ArrayAccessFromProperties;
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ViewModel;
+
+final class Altmetric implements ViewModel
+{
+    use ArrayAccessFromProperties;
+    use ArrayFromProperties;
+
+    private $doi;
+
+    public function __construct(
+        string $doi
+    ) {
+        Assertion::notBlank($doi);
+    }
+
+    public function getTemplateName() : string
+    {
+        return 'resources/templates/altmetric.mustache';
+    }
+}
