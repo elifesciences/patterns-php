f4f3c8993b0d4a46048d397ef3dabe316ea923f0
LinaKind <l.kinduryte@elifesciences.org>
2025-08-26 17:56:54 +0300

    Begin to extend the home banner view model and document with a failing test
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1281

--- src/ViewModel/HomeBanner.php
+++ src/ViewModel/HomeBanner.php
@@ -11,6 +11,18 @@ final class HomeBanner implements ViewModel
     use ArrayAccessFromProperties;
     use ArrayFromProperties;
 
+    private $primaryButton;
+    private $secondaryButton;
+
+    public function __construct(
+        Button $primaryButton,
+        Button $secondaryButton
+    ) {
+
+        $this->primaryButton = $primaryButton;
+        $this->secondaryButton = $secondaryButton;
+    }
+
     public function getTemplateName() : string
     {
         return 'resources/templates/home-banner.mustache';
--- tests/src/ViewModel/HomeBannerTest.php
+++ tests/src/ViewModel/HomeBannerTest.php
@@ -2,8 +2,10 @@
 
 namespace src\ViewModel;
 
+use eLife\Patterns\ViewModel\Button;
 use eLife\Patterns\ViewModel\HomeBanner;
 use tests\eLife\Patterns\ViewModel\ViewModelTest;
+use eLife\Patterns\ViewModel;
 
 final class HomeBannerTest extends ViewModelTest
 {
@@ -12,15 +14,33 @@ final class HomeBannerTest extends ViewModelTest
      */
     public function it_has_data()
     {
-        $homeBanner = new HomeBanner();
+        $primaryButton = Button::link('Primary button', '#');
+        $secondaryButton = Button::link('Secondary button', '#');
+        $homeBanner = new HomeBanner($primaryButton, $secondaryButton);
 
-        $this->assertSame([], $homeBanner->toArray());
+        $data = [
+            'primaryButton' => [
+                'classes' => 'button--default home-page__button',
+                'path' => '#',
+                'text' => 'Primary button',
+            ],
+            'secondaryButton' => [
+                'classes' => 'button--default home-page__button',
+                'path' => '#',
+                'text' => 'Secondary button',
+            ]
+            ];
+
+        $this->markTestIncomplete('failing test');
+        $this->assertSame($data, $homeBanner->toArray());
     }
 
     public function viewModelProvider() : array
     {
+        $primaryButton = Button::link('Primary button', '#');
+        $secondaryButton = Button::link('Secondary button', '#');
         return [
-            'complete' => [new HomeBanner()],
+            'complete' => [new HomeBanner($primaryButton, $secondaryButton)],
         ];
     }
 
