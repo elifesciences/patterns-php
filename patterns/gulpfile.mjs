e577ddb8c165af72af6a9b5f1fb5fe5c55c270c6
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-02-10 11:30:37 +0000

    Find all the test files in the test task and print
    https://github.com/elifesciences/issues/issues/9392

--- patterns/gulpfile.mjs
+++ patterns/gulpfile.mjs
@@ -1,30 +1,30 @@
-import gulp from 'gulp';
-import { deleteAsync } from 'del';
-import browserify from 'browserify';
-import babelify from 'babelify';
-import source from 'vinyl-source-stream';
-import buffer from 'vinyl-buffer';
-import sourcemaps from 'gulp-sourcemaps';
-import uglify from 'gulp-uglify';
-import rename from 'gulp-rename';
-import gulpSass from 'gulp-sass';
-import * as dartSass from 'sass';
-import sassGlob from 'gulp-sass-glob';
-import autoprefixer from 'gulp-autoprefixer';
-import imagemin from 'gulp-imagemin';
-import minimist from 'minimist';
-import through2 from 'through2';
+import gulp from "gulp";
+import { deleteAsync } from "del";
+import browserify from "browserify";
+import babelify from "babelify";
+import source from "vinyl-source-stream";
+import buffer from "vinyl-buffer";
+import sourcemaps from "gulp-sourcemaps";
+import uglify from "gulp-uglify";
+import rename from "gulp-rename";
+import gulpSass from "gulp-sass";
+import * as dartSass from "sass";
+import sassGlob from "gulp-sass-glob";
+import autoprefixer from "gulp-autoprefixer";
+import imagemin from "gulp-imagemin";
+import minimist from "minimist";
+import through2 from "through2";
 
 const sass = gulpSass(dartSass);
 // const browserSync = browserSyncLib.create();
 
 // Options / Environment
 const argv = minimist(process.argv.slice(2));
-const isProd = argv.environment === 'production';
+const isProd = argv.environment === "production";
 
 // Paths
-const srcDir = './assets';
-const destDir = './source/assets';
+const srcDir = "./assets";
+const destDir = "./source/assets";
 const paths = {
   jsMain: `${srcDir}/js/main.js`,
   jsLoader: `${srcDir}/js/elife-loader.js`,
@@ -46,18 +46,20 @@ const paths = {
 // 1. CSS Task
 export const generateCss = async () => {
   await deleteAsync([`${paths.cssDest}/*`]);
-  return gulp.src(paths.sassMain)
+  return gulp
+    .src(paths.sassMain)
     .pipe(sourcemaps.init())
     .pipe(sassGlob())
-    .pipe(sass({
-      style: isProd ? 'compressed' : 'expanded'
-    }).on('error', sass.logError))
+    .pipe(
+      sass({
+        style: isProd ? "compressed" : "expanded",
+      }).on("error", sass.logError),
+    )
     .pipe(autoprefixer())
-    .pipe(rename('all.css'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(paths.cssDest))
-    // .pipe(browserSync.stream())
-    ;
+    .pipe(rename("all.css"))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest(paths.cssDest));
+  // .pipe(browserSync.stream())
 };
 
 // 2. JS Task
@@ -66,25 +68,25 @@ export const js = async () => {
   gulp.src(paths.jsLoader).pipe(gulp.dest(paths.jsDest));
 
   return browserify(paths.jsMain, { debug: true })
-    .transform(babelify, { presets: ['@babel/preset-env'] })
+    .transform(babelify, { presets: ["@babel/preset-env"] })
     .bundle()
-    .on('error', function(err) {
+    .on("error", function (err) {
       console.error(err.message);
-      this.emit('end');
+      this.emit("end");
     })
-    .pipe(source('main.js'))
+    .pipe(source("main.js"))
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(isProd ? uglify() : through2.obj())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(paths.jsDest))
-    // .pipe(browserSync.stream())
-    ;
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest(paths.jsDest));
+  // .pipe(browserSync.stream())
 };
 
 // 3. Image Task
 export const images = () => {
-  return gulp.src(paths.imgSrc, { encoding: false })
+  return gulp
+    .src(paths.imgSrc, { encoding: false })
     .pipe(imagemin())
     .pipe(gulp.dest(paths.imgDest));
 };
@@ -92,8 +94,7 @@ export const images = () => {
 // 4. Fonts Task
 export const fonts = async () => {
   await deleteAsync([`${paths.fontsDest}/*`]);
-  return gulp.src(paths.fontsSrc)
-    .pipe(gulp.dest(paths.fontsDest));
+  return gulp.src(paths.fontsSrc).pipe(gulp.dest(paths.fontsDest));
 };
 
 // 5. preload
@@ -103,19 +104,26 @@ export const preload = async () => {
 
 // Watch Task
 export const watch = () => {
-  gulp.watch('assets/sass/**/*', generateCss);
-  gulp.watch('assets/js/**/*', js);
-  gulp.watch('assets/img/**/*', images);
-  gulp.watch('assets/fonts/**/*', fonts);
-  gulp.watch('assets/preload.json', preload);
+  gulp.watch("assets/sass/**/*", generateCss);
+  gulp.watch("assets/js/**/*", js);
+  gulp.watch("assets/img/**/*", images);
+  gulp.watch("assets/fonts/**/*", fonts);
+  gulp.watch("assets/preload.json", preload);
 };
 
 // Legacy pattern unit tests
-export const test = async () => {};
+export const test = async () => {
+  return gulp.src(`./test/*.html`).pipe(
+    through2.obj(function (file, _, cb) {
+      console.log(file.path);
+      cb(null, file);
+    }),
+  );
+};
 
 // Build / Default
 export const build = gulp.series(
-  gulp.parallel(generateCss, js, images, fonts, preload)
+  gulp.parallel(generateCss, js, images, fonts, preload),
 );
 
 export default build;
