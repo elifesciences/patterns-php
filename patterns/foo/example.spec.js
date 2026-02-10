c8c5f286b0dcaf85b6e0c27ea5088378cfc6964a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-02-10 15:52:20 +0000

    Change to a correct test expectation
    https://github.com/elifesciences/issues/issues/9392

--- patterns/foo/example.spec.js
+++ patterns/foo/example.spec.js
@@ -1,10 +1,12 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('to have 0 failures', async ({ page }) => {
-  await page.goto('file:///Users/linakinduryte/patterns-php/patterns/test/authors.html');
+test("to have 0 failures", async ({ page }) => {
+  await page.goto(
+    "file:///Users/linakinduryte/patterns-php/patterns/test/authors.html",
+  );
 
-  const failureCountLocator = page.locator('.failures em')
+  const failureCountLocator = page.locator(".failures em");
 
-  await expect(failureCountLocator).toHaveText("5");
+  await expect(failureCountLocator).toHaveText("0");
 });
