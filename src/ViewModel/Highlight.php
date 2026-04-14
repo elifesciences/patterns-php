9a649c64045887247a65ab64426f2cf5cf427b4e
Dragos <dmmargarint@gmail.com>
2026-04-14 15:25:22 +0100

    Run bin/update-from-local-patterns

--- src/ViewModel/Highlight.php
+++ src/ViewModel/Highlight.php
@@ -15,13 +15,36 @@ final class Highlight implements ViewModel
     private $heading;
     private $items;
 
-    public function __construct(array $items, ListHeading $heading = null)
+    private $heroItem;
+
+    public function __construct(array $items, ListHeading $heading = null, $heroItem = false)
     {
         Assertion::notEmpty($items);
         Assertion::allIsInstanceOf($items, HighlightItem::class);
 
         $this->heading = $heading;
-        $this->items = $items;
+
+        /**
+         * If $heroItem is true, set the first item as the hero item
+         * Else set the all items per usual
+         */
+        if ($heroItem) {
+            $this->heroItem = array_slice($items, 0, 1);
+            $this->items = array_slice($items, 1);
+        } else {
+            $this->items = $items;
+        }
+    }
+
+    /**
+     * Fluent setter of $heroItem
+     * @param HighlightItem $item
+     * @return $this
+     */
+    public function withHeroItem(HighlightItem $item): self
+    {
+        $this->heroItem = $item;
+        return $this;
     }
 
     public function getTemplateName() : string
